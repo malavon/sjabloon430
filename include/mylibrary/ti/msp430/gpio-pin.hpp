43a96d8a3c793e9211fa7d9f2bb281dca0692cab
Benny Goemans <benny.goemans@wacadu.com>
2017-02-11 22:44:57 +0100

    Added templates for gpio with and without interrupt + init
    Looks like the sample app didn't change at all except for the init - which
    is necessary as far as the datasheet goes.
    
    While doing this I've added the full GPIO 1 & 2 for the MSP430G2553
    device. This is useful anyway.
    
    Note: we can't call the init method from the GpioPort constructor, it
    pulls in class-related bits which we want to avoid. Why this is different
    from the Button and Led classes isn't clear to me right now.

--- include/mylibrary/ti/msp430/gpio-pin.hpp
+++ include/mylibrary/ti/msp430/gpio-pin.hpp
@@ -4,48 +4,95 @@
 #include "gpio-port.hpp"
 
 namespace mylibrary {
-template<vub& inReg, vub& outReg, vub& dirReg, vub& intReg, vub& intEdgeReg, vub& intEnReg, vub& selectReg, vub& resistorReg, class P, const P pin>
-class Pin: private GpioTemplate<inReg, outReg, dirReg, intReg, intEdgeReg, intEnReg, selectReg, resistorReg, P> {
+
+template<vub& inReg, vub& outReg, vub& dirReg, vub& selectReg, vub& resistorReg, class P, const P pin>
+class Pin: private GpioPort<inReg, outReg, dirReg, selectReg, resistorReg, P> {
 public:
     Pin() {
     }
 
     void configureAsInput() const {
-        GpioTemplate<inReg, outReg, dirReg, intReg, intEdgeReg, intEnReg, selectReg, resistorReg, P>::configureAsInput(pin);
+        GpioPort<inReg, outReg, dirReg, selectReg, resistorReg, P>::configureAsInput(pin);
     }
 
     void configureAsOutput() const {
-        GpioTemplate<inReg, outReg, dirReg, intReg, intEdgeReg, intEnReg, selectReg, resistorReg, P>::configureAsOutput(pin);
+        GpioPort<inReg, outReg, dirReg, selectReg, resistorReg, P>::configureAsOutput(pin);
     }
 
     void enablePulldown() const {
-        GpioTemplate<inReg, outReg, dirReg, intReg, intEdgeReg, intEnReg, selectReg, resistorReg, P>::enablePulldown(pin);
+        GpioPort<inReg, outReg, dirReg, selectReg, resistorReg, P>::enablePulldown(pin);
     }
 
     void enablePullup() const {
-        GpioTemplate<inReg, outReg, dirReg, intReg, intEdgeReg, intEnReg, selectReg, resistorReg, P>::enablePullup(pin);
+        GpioPort<inReg, outReg, dirReg, selectReg, resistorReg, P>::enablePullup(pin);
     }
 
     bool isLow() const {
-        return GpioTemplate<inReg, outReg, dirReg, intReg, intEdgeReg, intEnReg, selectReg, resistorReg, P>::isLow(pin);
+        return GpioPort<inReg, outReg, dirReg, selectReg, resistorReg, P>::isLow(pin);
     }
 
     bool isHigh() const {
-        return GpioTemplate<inReg, outReg, dirReg, intReg, intEdgeReg, intEnReg, selectReg, resistorReg, P>::isHigh(pin);
+        return GpioPort<inReg, outReg, dirReg, selectReg, resistorReg, P>::isHigh(pin);
     }
 
     void setLow() const {
-        GpioTemplate<inReg, outReg, dirReg, intReg, intEdgeReg, intEnReg, selectReg, resistorReg, P>::setLow(pin);
+        GpioPort<inReg, outReg, dirReg, selectReg, resistorReg, P>::setLow(pin);
     }
 
     void setHigh() const {
-        GpioTemplate<inReg, outReg, dirReg, intReg, intEdgeReg, intEnReg, selectReg, resistorReg, P>::setHigh(pin);
+        GpioPort<inReg, outReg, dirReg, selectReg, resistorReg, P>::setHigh(pin);
     }
 
     void toggle() const {
-        GpioTemplate<inReg, outReg, dirReg, intReg, intEdgeReg, intEnReg, selectReg, resistorReg, P>::toggle(pin);
+        GpioPort<inReg, outReg, dirReg, selectReg, resistorReg, P>::toggle(pin);
     }
 };
+
+template<vub& inReg, vub& outReg, vub& dirReg, vub& selectReg, vub& resistorReg, vub& intReg, vub& intEdgeReg, vub& intEnReg, class P, const P pin>
+class PinWithInterrupt: public Pin<inReg, outReg, dirReg, selectReg, resistorReg, P, pin>, private GpioPortWithInterrupt<inReg, outReg, dirReg, selectReg, resistorReg, intReg, intEdgeReg, intEnReg, P> {
+public:
+    PinWithInterrupt() {
+    }
+
+    // these methods need to be here to resolve ambiguity
+    void configureAsInput() const {
+        Pin<inReg, outReg, dirReg, selectReg, resistorReg, P, pin>::configureAsInput();
+    }
+
+    void configureAsOutput() const {
+        Pin<inReg, outReg, dirReg, selectReg, resistorReg, P, pin>::configureAsOutput();
+    }
+
+    void enablePulldown() const {
+        Pin<inReg, outReg, dirReg, selectReg, resistorReg, P, pin>::enablePulldown();
+    }
+
+    void enablePullup() const {
+        Pin<inReg, outReg, dirReg, selectReg, resistorReg, P, pin>::enablePullup();
+    }
+
+    bool isLow() const {
+        return Pin<inReg, outReg, dirReg, selectReg, resistorReg, P, pin>::isLow();
+    }
+
+    bool isHigh() const {
+        return Pin<inReg, outReg, dirReg, selectReg, resistorReg, P, pin>::isHigh();
+    }
+
+    void setLow() const {
+        Pin<inReg, outReg, dirReg, selectReg, resistorReg, P, pin>::setLow();
+    }
+
+    void setHigh() const {
+        Pin<inReg, outReg, dirReg, selectReg, resistorReg, P, pin>::setHigh();
+    }
+
+    void toggle() const {
+        Pin<inReg, outReg, dirReg, selectReg, resistorReg, P, pin>::toggle();
+    }
+};
+
 }
+
 
 #endif /* MYLIBRARY_TI_MSP430_GPIO_PIN_HPP_ */
--- include/mylibrary/ti/msp430/gpio-port.hpp
+++ include/mylibrary/ti/msp430/gpio-port.hpp
@@ -4,10 +4,22 @@
 #include "../includes.h"
 
 namespace mylibrary {
-template<vub& inReg, vub& outReg, vub& dirReg, vub& intReg, vub& intEdgeReg, vub& intEnReg, vub& selectReg, vub& resistorReg, class P>
-class GpioTemplate {
+
+template<vub& inReg, vub& outReg, vub& dirReg, vub& selectReg, vub& resistorReg, class P>
+class GpioPort {
 public:
-    GpioTemplate() {
+    GpioPort() {
+    }
+
+    void init() const {
+        /*
+         * Unused I/O pins should be configured as I/O function, output direction, and left unconnected on the PC
+         * board, to prevent a floating input and reduce power consumption. The value of the PxOUT bit is irrelevant,
+         * since the pin is unconnected. Alternatively, the integrated pullup/pulldown resistor can be enabled by
+         * setting the PxREN bit of the unused pin to prevent the floating input. See the System Resets, Interrupts,
+         * and Operating Modes chapter for termination of unused pins.
+         */
+        dirReg = 0xff;
     }
 
     void configureAsInput(const P pin1, const P pin2 = (P) 0, const P pin3 = (P) 0, const P pin4 = (P) 0, const P pin5 = (P) 0, const P pin6 = (P) 0, const P pin7 = (P) 0,
@@ -21,7 +33,9 @@ public:
     }
 
     void enablePullup(const P pin1, const P pin2 = (P) 0, const P pin3 = (P) 0, const P pin4 = (P) 0, const P pin5 = (P) 0, const P pin6 = (P) 0, const P pin7 = (P) 0, const P pin8 = (P) 0) const {
+        // Bit = 0: The pin is pulled down, Bit = 1: The pin is pulled up
         outReg |= pin1 | pin2 | pin3 | pin4 | pin5 | pin6 | pin7 | pin8;
+        // Bit = 0: Pullup/pulldown resistor disabled, Bit = 1: Pullup/pulldown resistor enabled
         resistorReg |= pin1 | pin2 | pin3 | pin4 | pin5 | pin6 | pin7 | pin8;
     }
 
@@ -31,11 +45,11 @@ public:
     }
 
     bool isLow(const P pin) const {
-        return inReg & pin == 0;
+        return (inReg & pin) == 0;
     }
 
     bool isHigh(const P pin) const {
-        return inReg & pin == pin;
+        return (inReg & pin) == pin;
     }
 
     void setLow(const P pin1, const P pin2 = (P) 0, const P pin3 = (P) 0, const P pin4 = (P) 0, const P pin5 = (P) 0, const P pin6 = (P) 0, const P pin7 = (P) 0, const P pin8 = (P) 0) const {
@@ -49,6 +63,17 @@ public:
     void toggle(const P pin1, const P pin2 = (P) 0, const P pin3 = (P) 0, const P pin4 = (P) 0, const P pin5 = (P) 0, const P pin6 = (P) 0, const P pin7 = (P) 0, const P pin8 = (P) 0) const {
         outReg ^= (pin1 | pin2 | pin3 | pin4 | pin5 | pin6 | pin7 | pin8);
     }
+};
+
+
+
+template<vub& inReg, vub& outReg, vub& dirReg, vub& selectReg, vub& resistorReg, vub& intReg, vub& intEdgeReg, vub& intEnReg, class P>
+class GpioPortWithInterrupt: public GpioPort<inReg, outReg, dirReg, selectReg, resistorReg, P> {
+public:
+    GpioPortWithInterrupt() {
+    }
+
+    // TODO: interrupt stuff
 };
 
 } // namespace mylibrary
