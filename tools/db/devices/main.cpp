ad7c49a130104d3e5ea0f0f506f714a6b1ffeb84
Benny Goemans <git@malavon.eu>
2024-12-19 01:35:02 +0100

    DB Tool device: basic usage

--- tools/db/devices/main.cpp
+++ tools/db/devices/main.cpp
@@ -1,37 +1,67 @@
 #include <iostream>
 
+#include <getopt.h>
+#include "sqlite3.h"
+
 using namespace std;
+
+void printUsage() {
+	cout << "Usage: devices [-h] "
+		 << "[-d <datasheets-file>] "
+		 << "[-f <families-file>] "
+		 << "[-l <links-file>] "
+		 << "[-p <packages-file>] <sqlite3db>" << endl;
+
+	cout << "<families-file> is a tab-separated file eported from the TI website "
+	 "and converted to tsv containing records of:\n"
+		 << "\tPart\tFrequency\tNonvolatile Memory\tRAM\tADC Type etc\n\n";
+
+	cout << "<datasheets-file> is a tab-separated file containing records of:\n"
+		 << "\tSheet\tRev.\tIssued\tRevised\n"
+		 << "e.g.\tSLAS380\tF\t4/2004\t3/2022\n\n";
+
+	cout << "<links-file> is a tab-separated file containing records of:\n"
+		 << "\tPart Number\tSheet\n"
+		 << "e.g.\tMSP430FR2533\tSLAS942\n\n";
+
+	cout << "<packages-file> is a tab-separated file containing records of:\n"
+		 << "\tOrderable\t\tSheet\tStatus\tPackage\tDrawing\t# Pins\tMSP\n"
+		 << "e.g.\tMSP430FR2533IRHBT\tslas942\tACTIVE\tVQFN\tRHB\t32\tLevel-2-260C-1_YEAR\n";
+}
 
 int main() // opties voor elke .txt file? misschien niet slecht?
 {
-    // read msp430-family.txt for devices (export van TI website! niet modificeren)
-    // read packages.txt (extracted from datasheets)
-    // extend packages.txt? niet ieder DS heeft devices in header; meeste wel
-    //  - mogelijk: proberen, indien 1 optie: OK, anders vragen?
-    //  - of beter?: lijst van links, die resolutie zou 99% ok moeten zijn op dit moment
-    //      - wiel niet opnieuw uitvinden, maar geen manuele verificatie op deze manier?
-    // MAAR: zitten toch nog altijd met de matching van mcu & package
-    // EN: niet elke match is op naam te doen!
-    // OPLOSSING: derde txt file aangemaakt met device & datasheet
-    // for family
-    {
-        // insert or update in device table
-        // comment: TI EXPORT
-    }
-    // for datasheet links
-    {
-        // insert or update in device table
-        // comment: AUTOMATIC RESOLUTION (!= MANUALLY VERIFIED)
-    }
-    // for datasheets (rev & dates)
-    {
-        // insert (or update?) in device table
-    }
-    // for packages
-    {
-        // insert package & use datasheet to resolve family + part
-        // comment: AUTOMATIC RESOLUTION (!= MANUALLY VERIFIED)
-    }
-    return 0;
-}
+	printUsage();
+	sqlite3 *sql;
+	// read msp430-family.txt for devices (export van TI website! niet modificeren)
+	// read packages.txt (extracted from datasheets)
+	// extend packages.txt? niet ieder DS heeft devices in header; meeste wel
+	//  - mogelijk: proberen, indien 1 optie: OK, anders vragen?
+	//  - of beter?: lijst van links, die resolutie zou 99% ok moeten zijn op dit moment
+	//      - wiel niet opnieuw uitvinden, maar geen manuele verificatie op deze manier?
+	// MAAR: zitten toch nog altijd met de matching van mcu & package
+	// EN: niet elke match is op naam te doen!
+	// OPLOSSING: derde txt file aangemaakt met device & datasheet
+	// for family
+	{
+		// insert or update in device table
+		// comment: TI EXPORT
+	}
+	// for datasheet links
+	{
+		// insert or update in device table
+		// comment: AUTOMATIC RESOLUTION (!= MANUALLY VERIFIED)
+	}
+	// for datasheets (rev & dates)
+	{
+		// insert (or update?) in device table
+	}
+	// for packages
+	{
+		// insert package & use datasheet to resolve family + part
+		// comment: AUTOMATIC RESOLUTION (!= MANUALLY VERIFIED)
+	}
 
+	// sqlite3_close(sql);
+	return 0;
+}
