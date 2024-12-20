4d234b9eb51456294f55c676115f88f91b677cff
Benny Goemans <git@malavon.eu>
2024-12-20 21:32:11 +0100

    DB: SQLite3 database to auto-generate code from datasheets' contents
    The main idea of the tools to use this data is to generate the necessary header
    files for each device, making it much easier to extend support to a large
    number of different parts without risking mistakes from manual use.
    Also, it's a good idea to gain some experience with C & C++ libraries.
    
    Tables are self-explanatory, but still some explanations:
    - datasheet contains SLAS-numbers and revision versions/dates
        This makes matching of pins easier, since pretty much only devices in the
        same datasheet share common pinouts.
    - device contains data from a list of devices exported from the TI website and
      then modified a bit for easier parsing, exported again into a tab-separated
      file.
        This is actually a dumbed-down AND INCOMPLETE listing of features, but it's
        a first start aside from parsing datasheets, most likely at least partially
        manually.
    - feature contains keys that are found in the TI export under the "Features"
      and "Special I/O" headers, amongst others.
        It should allow us to actually enable certain code only if a certain feature
        is present (e.g. ADC) etc. It has parameters because some features will
        require these without having to add a different Feature for each variation.
    - device_feature is a rather simple link table between device & feature, also
      including parameter values.

---
+++ tools/db/12_feature.sql
@@ -0,0 +1,32 @@
+--
+-- File generated with SQLiteStudio.
+--
+-- Text encoding used: UTF-8
+--
+
+-- Table: feature
+CREATE TABLE IF NOT EXISTS feature (
+    id                 TEXT PRIMARY KEY,
+    param1_description TEXT,
+    param2_description ANY,
+    param3_description ANY,
+    comment            TEXT,
+    family_text        TEXT,
+    family_group       TEXT
+)
+WITHOUT ROWID,
+STRICT;
+
+-- Table: device_feature
+CREATE TABLE IF NOT EXISTS device_feature (
+    device_id  INTEGER REFERENCES device (id) ON DELETE RESTRICT
+                       NOT NULL,
+    feature_id TEXT    REFERENCES feature (id) ON DELETE RESTRICT
+                       NOT NULL,
+    param1     ANY,
+    param2     ANY,
+    param3     ANY,
+    comment    TEXT
+)
+STRICT;
+
