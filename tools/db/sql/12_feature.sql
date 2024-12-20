dde523eff2912e0c5fa166b752053901fe2869c2
Benny Goemans <git@malavon.eu>
2024-12-20 22:39:02 +0100

    DB: features inserted manually
    This is a basic, most likely incomplete list of existing features. The features
    here are the ones present in the MSP430 family TSV file.
    Most likely more features or more variants of features will be added later on.

--- tools/db/sql/12_feature.sql
+++ tools/db/sql/12_feature.sql
@@ -12,21 +12,22 @@ CREATE TABLE IF NOT EXISTS feature (
     param3_description ANY,
     comment            TEXT,
     family_text        TEXT,
-    family_group       TEXT
+    family_group       TEXT,
+    signal_group       TEXT REFERENCES signalgroup (name)
 )
 WITHOUT ROWID,
 STRICT;
 
 -- Table: device_feature
 CREATE TABLE IF NOT EXISTS device_feature (
-    device_id  INTEGER REFERENCES device (id) ON DELETE RESTRICT
-                       NOT NULL,
-    feature_id TEXT    REFERENCES feature (id) ON DELETE RESTRICT
-                       NOT NULL,
+    device_id  TEXT NOT NULL
+                    REFERENCES device (model) ON DELETE CASCADE
+                                              ON UPDATE CASCADE,
+    feature_id TEXT REFERENCES feature (id) ON DELETE RESTRICT
+                    NOT NULL,
     param1     ANY,
     param2     ANY,
     param3     ANY,
     comment    TEXT
 )
 STRICT;
-
