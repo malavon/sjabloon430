b3d4546595ca5ae0c00b049dadb69d154e8f93bd
Benny Goemans <git@malavon.eu>
2024-12-21 05:28:16 +0100

    DB Tools Device: read families & datasheet links + populate insert query

--- tools/device/main.cpp
+++ tools/device/main.cpp
@@ -3,6 +3,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <unordered_map>
 
 #include "sqlite3.h"
 
@@ -32,12 +33,12 @@ void printUsage() {
 	     << "e.g.\tMSP430FR2533IRHBT\tslas942\tACTIVE\tVQFN\tRHB\t32\tLevel-2-260C-1_YEAR\n";
 }
 
-void insertOrUpdateFamilies(sqlite3 *db, const ifstream &families, const ifstream &links);
-void insertOrUpdatePackages(sqlite3 *db, const ifstream &packages);
+void insertOrUpdateFamilies(sqlite3 *db, ifstream &families, ifstream &links);
+void insertOrUpdatePackages(sqlite3 *db, ifstream &packages);
 
 const string INSERT_DEVICE = "INSERT INTO device (datasheet_id,model,freq_max,storage_bytes,"
 			     "ram_bytes,gpio_count,uart_count,usb_count,i2c_count,spi_count,"
-			     "timer_count,op_temp_min,op_temp_max,comment) VALUES";
+			     "comp_count,timer_count,op_temp_min,op_temp_max,comment) VALUES ";
 
 int main(int argc, char **argv) // opties voor elke .txt file? misschien niet slecht?
 {
@@ -49,7 +50,7 @@ int main(int argc, char **argv) // opties voor elke .txt file? misschien niet sl
 	const char *database = nullptr;
 
 	int opt;
-	while ( (opt = getopt(argc, argv, "dflph")) != -1 ) {
+	while ( (opt = getopt(argc, argv, "d:f:l:p:h")) != -1 ) {
 		switch ( opt ) {
 			// case 'd':
 			// 	datasheets = optarg;
@@ -69,9 +70,9 @@ int main(int argc, char **argv) // opties voor elke .txt file? misschien niet sl
 				return EX_USAGE;
 		}
 	}
-
-	if ( argc > optind + 1 ) {
-		database = argv[optind + 1];
+	cout << "argc " << argc << " & optind " << optind << endl;
+	if ( argc > optind ) {
+		database = argv[optind];
 	} else {
 		printUsage();
 		return EX_USAGE;
@@ -101,39 +102,145 @@ int main(int argc, char **argv) // opties voor elke .txt file? misschien niet sl
 	if ( families.is_open() && links.is_open() ) {
 		insertOrUpdateFamilies(sql, families, links);
 	} else {
-		cout << "Could not read ";
-		if ( !families.is_open() ) {
-			cout << "families ";
-			if ( links.is_open() ) {
-				cout << "file ";
-			} else {
-				cout << "and links files";
-			}
-			cout << endl;
-			return EX_DATAERR;
-		}
+		cout << "No families & links read " << endl;
 	}
 
 	if ( packages.is_open() ) {
 		insertOrUpdatePackages(sql, packages);
 	} else {
-		cout << "Could not read packages file" << endl;
-		return EX_DATAERR;
+		cout << "No packages read" << endl;
 	}
 
 	sqlite3_close(sql);
 	return 0;
 }
 
-void insertOrUpdateFamilies(sqlite3 *db, const ifstream &families, const ifstream &links) {
-	// for family
-	{
+void addNextString(string &query, string value) {
+	query.push_back('\'');
+	query.append(value);
+	query.push_back('\'');
+	query.push_back(',');
+}
+
+void addNextInteger(string &query, string value) {
+	// no quotes, numbers are simply inserted (even though they are floating-point)
+	query.append(value.empty() ? "0" : value);
+	query.push_back(',');
+}
+
+string nextTSV(string &line, unsigned int &tab1, unsigned int &tab2) {
+	unsigned int temp = tab1;
+	tab2 = line.find('\t', tab1);
+	tab1 = tab2 + 1;
+	return line.substr(temp, tab2 - temp);
+}
+
+void insertOrUpdateFamilies(sqlite3 *db, ifstream &families, ifstream &links) {
+	string line;
+	// read links in map first
+
+	unordered_map<string, string> datasheets;
+	while ( getline(links, line) ) {
+		// expected <device>\t<datasheet>
+		// e.g. MSP430FR2533	SLAS942
+		// abuse the fact that datasheets are always 7 characters long
+		// preceded by a single character (tab)
+		unsigned int l = line.length();
+		datasheets[line.substr(0, l - 8)] = line.substr(l - 7);
+		// cout << "datasheet '" << line.substr(l - 7) << "' for device '" << line.substr(0, l - 8) << "'" << endl;
+	}
+
+	unsigned int tab1, tab2;
+	string value;
+	// no, this isn't efficient, but it's fine for this program
+	string query;
+	// first line contains header, but no datasheet will be found anyway
+	while ( getline(families, line) ) {
+		tab1 = 0;
+		query = INSERT_DEVICE;
+		query.append("( ");
+
+		// Part & Datasheet!!
+		value = nextTSV(line, tab1, tab2);
+		// if NULL, should fail
+		addNextString(query, datasheets[value]);
+		addNextString(query, value);
+		// tab1 = line.find('\t', 0);
+		// query.append("( \"");
+		// query.append(line.substr(0, tab1));
+		// query.push_back('"');
+
+		// Frequency (MHz)
+		addNextInteger(query, nextTSV(line, tab1, tab2));
+		// value memory (kByte)
+		// TODO: replace 0.13 with 0.125 ...
+		value = nextTSV(line, tab1, tab2);
+		value += "*1024";
+		addNextInteger(query, value);
+		// RAM (kByte)
+		value = nextTSV(line, tab1, tab2);
+		if ( value == "0.13" ) {
+			addNextInteger(query, "0.125*1024");
+		} else {
+			value += "*1024";
+			addNextInteger(query, value);
+		}
+		// ADC type
+		nextTSV(line, tab1, tab2);
+		// TODO: interpret & add features
+		// Number of ADC channels
+		nextTSV(line, tab1, tab2);
+
+		// Number of GPIOs
+		addNextInteger(query, nextTSV(line, tab1, tab2));
+
+		// UART
+		addNextInteger(query, nextTSV(line, tab1, tab2));
+
+		// USB
+		value = nextTSV(line, tab1, tab2);
+		if ( value == "Yes" ) {
+			addNextInteger(query, "1");
+		} else {
+			addNextInteger(query, "0");
+		}
+
+		// Number of I2Cs
+		addNextInteger(query, nextTSV(line, tab1, tab2));
+		// SPI
+		addNextInteger(query, nextTSV(line, tab1, tab2));
+		// Number of comparator channels
+		addNextInteger(query, nextTSV(line, tab1, tab2));
+		// Timers - 16-bit
+		addNextInteger(query, nextTSV(line, tab1, tab2));
+
+		// Bootloader (BSL)
+		// TODO: features
+		nextTSV(line, tab1, tab2);
+		// Special I/O
+		// TODO: features
+		nextTSV(line, tab1, tab2);
+
+		// Operating temperature range (°C)
+		// (-)n(n) to mm(m)
+		value = nextTSV(line, tab1, tab2);
+		tab2 = value.find(" to ");
+		addNextInteger(query, value.substr(0, tab2));
+		addNextInteger(query, value.substr(tab2 + 4, value.length() - tab2 - 4));
+
+		// Price|Quantity (USD)	Package type	Pin count	Package area (mm^2)	Package
+		// size (L x W) (mm)	Features
+
+		addNextString(query, "AUTOMATIC RESOLUTION");
+
 		// insert or update in device table
 		// comment: TI EXPORT
+		query[query.length() - 1] = ')';
+		cout << query << endl;
 	}
 }
 
-void insertOrUpdatePackages(sqlite3 *db, const ifstream &packages) {
+void insertOrUpdatePackages(sqlite3 *db, ifstream &packages) {
 	// for packages
 	{
 		// insert package & use datasheet to resolve family + part
