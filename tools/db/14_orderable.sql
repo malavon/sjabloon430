d6885ff4c26a26128d9242284eb4c33a8d2dbc69
Benny Goemans <git@malavon.eu>
2024-12-22 21:54:33 +0100

    DB: First version of package & orderable TODO KEEP?
    Both orderable and package are useful to keep:
    
    - package is quite obvious, since there should be at least some reference
      about the amount of pins a certain device has along with which style. Not
      every MSP430 comes in the same packages - by far.
      In this version package contains the link to device so as not to have another
      link table. However, this means that the same package is inserted into the
      database for each device - of which there are many. This could be avoided if
      the link is contained outside of this table.
    
    - orderable has a link to package and thus implicitly to device & datasheet. In
      theory orderable isn't really necessary to keep, I can't think of many cases
      in which it's important to know in which ways a certain part can be ordered
      (e.g. tray, tube etc) and under which names.
      However: it is from the orderable data in the datasheets that device/package
      links are built. This linking had to happen according to certain rules, which
      makes it difficult to ensure everything is correct. For this reason alone
      orderable proved useful to ensure correctness in the tools.
    
    Note: this first version is here only for historical purposes and to document
    at least part of the thinking process behind later changes.

---
+++ tools/db/14_orderable.sql
@@ -0,0 +1,21 @@
+--
+-- File generated with SQLiteStudio v3.4.4, modified manually!
+--
+-- Text encoding used: UTF-8
+--
+BEGIN TRANSACTION;
+
+-- Table: orderable
+CREATE TABLE IF NOT EXISTS orderable (
+    name       TEXT    CONSTRAINT UQ_ORDERABLE_NAME UNIQUE ON CONFLICT ROLLBACK
+                       CONSTRAINT NN_ORDERABLE_NAME NOT NULL
+                       PRIMARY KEY,
+    package_id INTEGER CONSTRAINT NN_ORDERABLE_PACKAGE_ID NOT NULL
+                       CONSTRAINT FK_ORDERABLE_PACKAGE REFERENCES package (id) ON DELETE RESTRICT,
+    status     TEXT    NOT NULL,
+    msl_level  INTEGER,
+    comment    TEXT
+)
+STRICT;
+
+COMMIT TRANSACTION;
