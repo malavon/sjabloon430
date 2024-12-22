db2a0fc2c9b919f32dd88e0e6debb133fdb9df72
Benny Goemans <git@malavon.eu>
2024-12-22 21:54:33 +0100

    DB: package split into package & orderable TODO reword rationale NO DATA REWORD
    RATIO

--- tools/db/msp430.sql
+++ tools/db/msp430.sql
@@ -1,5 +1,9 @@
 --
+<<<<<<< HEAD
 -- File generated with SQLiteStudio v3.4.4
+=======
+-- File generated with SQLiteStudio v3.4.4 on Mon Dec 23 03:06:53 2024
+>>>>>>> 8bde3fd (DB: package split into package & orderable TODO reword rationale)
 --
 -- Text encoding used: UTF-8
 --
@@ -2994,6 +2998,38 @@ INSERT INTO feature (id, param1_description, param2_description, param3_descript
 INSERT INTO feature (id, param1_description, param2_description, param3_description, comment, family_text, family_group) VALUES ('USCI_A_LIN', 'TODO: not all IrDA?', NULL, NULL, 'USCI with IrDA, UART, SPI', NULL, NULL);
 INSERT INTO feature (id, param1_description, param2_description, param3_description, comment, family_text, family_group) VALUES ('USCI_B', NULL, NULL, NULL, 'USCI with I²C, SPI', NULL, NULL);
 INSERT INTO feature (id, param1_description, param2_description, param3_description, comment, family_text, family_group) VALUES ('WTD+', NULL, NULL, NULL, NULL, 'Watchdog timer', 'Features');
+
+-- Table: orderable
+CREATE TABLE IF NOT EXISTS orderable (
+    name       TEXT    CONSTRAINT UQ_ORDERABLE_NAME UNIQUE ON CONFLICT ROLLBACK
+                       CONSTRAINT NN_ORDERABLE_NAME NOT NULL
+                       PRIMARY KEY,
+    package_id INTEGER CONSTRAINT NN_ORDERABLE_PACKAGE_ID NOT NULL
+                       CONSTRAINT FK_ORDERABLE_PACKAGE REFERENCES package (id) ON DELETE RESTRICT,
+    status     TEXT    NOT NULL,
+    msl_level  INTEGER,
+    comment    TEXT
+)
+STRICT;
+
+
+-- Table: package
+CREATE TABLE IF NOT EXISTS package (
+    id        INTEGER PRIMARY KEY ON CONFLICT ROLLBACK AUTOINCREMENT
+                      NOT NULL,
+    device_id INTEGER REFERENCES device (id) ON DELETE CASCADE,
+    type      TEXT    NOT NULL,
+    drawing   TEXT    NOT NULL,
+    pins      INTEGER NOT NULL,
+    comment   TEXT,
+    CONSTRAINT UQ_PACKAGE_DEV_DRW_PINS UNIQUE (
+        device_id,
+        drawing,
+        pins
+    )
+    ON CONFLICT ROLLBACK
+)
+STRICT;
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
