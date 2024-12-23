f058cf7b87d58dfb2ce21552a73fac50a3f715d5
Benny Goemans <git@malavon.eu>
2024-12-23 04:15:18 +0100

    DB: added signal  & package_signal TODO preliminary?
    this is what it was all about, pin signals to use for auto-generation

---
+++ tools/db/15_signal.sql
@@ -0,0 +1,32 @@
+--
+-- File generated with SQLiteStudio v3.4.4, modified manually!
+--
+-- Text encoding used: UTF-8
+--
+BEGIN TRANSACTION;
+
+-- Table: signal
+CREATE TABLE IF NOT EXISTS signal (
+    id          INTEGER PRIMARY KEY ON CONFLICT ROLLBACK AUTOINCREMENT
+                        NOT NULL,
+    signal_name TEXT    NOT NULL,
+    signal_type TEXT    NOT NULL
+)
+STRICT;
+
+-- Table: package_signal
+CREATE TABLE IF NOT EXISTS package_signal (
+    package_id  INTEGER REFERENCES package (id) ON DELETE RESTRICT
+                        NOT NULL,
+    signal_id   INTEGER NOT NULL
+                        REFERENCES signal (id) ON DELETE RESTRICT
+                                               ON UPDATE RESTRICT,
+    pin_row_bga TEXT,
+    pin_number  INTEGER NOT NULL,
+    is_default  INTEGER DEFAULT (0) 
+                        NOT NULL,
+    comment     TEXT
+)
+STRICT;
+
+COMMIT TRANSACTION;
