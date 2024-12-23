05c1c6504833ff393dd7fdfebaa96d2e0e173cba
Benny Goemans <git@malavon.eu>
2024-12-23 05:16:12 +0100

    DB: EXPEROMENT signal feature group more important

--- tools/db/15_signal.sql
+++ tools/db/15_signal.sql
@@ -6,10 +6,11 @@
 
 -- Table: signal
 CREATE TABLE IF NOT EXISTS signal (
-    id          INTEGER PRIMARY KEY ON CONFLICT ROLLBACK AUTOINCREMENT
-                        NOT NULL,
-    signal_name TEXT    NOT NULL,
-    signal_type TEXT    NOT NULL
+    id                   INTEGER PRIMARY KEY ON CONFLICT ROLLBACK AUTOINCREMENT
+                                 NOT NULL,
+    signal_name          TEXT    NOT NULL,
+    signal_desc          TEXT    NOT NULL,
+    signal_feature_group TEXT    CONSTRAINT FK_SIGNAL_FEATURE REFERENCES feature (signal_group) 
 )
 STRICT;
 
@@ -24,7 +25,13 @@ CREATE TABLE IF NOT EXISTS package_signal (
     pin_number  INTEGER NOT NULL,
     is_default  INTEGER DEFAULT (0) 
                         NOT NULL,
-    comment     TEXT
+    comment     TEXT,
+    CONSTRAINT PK_PACKAGE_SIGNAL PRIMARY KEY (
+        package_id,
+        signal_id
+    )
+    ON CONFLICT FAIL
 )
+WITHOUT ROWID,
 STRICT;
 
