ef361883a69365d8e3916273c549566ff07a0044
Benny Goemans <git@malavon.eu>
2024-12-28 04:16:32 +0100

    DB: experiment using orderable as pinset link table
    package_pinset no longer needed
    and pinsets can be reused as well, doesn't depend on package
     is more ad-hoc even, different part, even different SLASs can share
     with filter
    also package_signal deleted? already done before I think ... TODO
    
    parent -> additive pinset

---
+++ tools/db/15_pinset.sql
@@ -0,0 +1,40 @@
+--
+-- File generated with SQLiteStudio, comment modified manually!
+--
+-- Text encoding used: UTF-8
+--
+
+-- Table: pinset
+CREATE TABLE IF NOT EXISTS pinset (
+    id            INTEGER CONSTRAINT PK_SET PRIMARY KEY AUTOINCREMENT,
+    parent_set_id ANY     CONSTRAINT FK_pinset_pinset REFERENCES pinset (id) ON DELETE RESTRICT,
+    pins          INTEGER NOT NULL,
+    comment       TEXT
+)
+STRICT;
+
+-- Table: pinset_signal
+CREATE TABLE IF NOT EXISTS pinset_signal (
+    pinset_id   INTEGER REFERENCES pinset (id) ON DELETE RESTRICT
+                        NOT NULL,
+    signal_id   INTEGER NOT NULL
+                        REFERENCES signal (id) ON DELETE RESTRICT
+                                               ON UPDATE RESTRICT,
+    idx         INTEGER CONSTRAINT NN_SIGNAL_SET_INDEX NOT NULL,
+    pin_row_bga TEXT,
+    pin_number  INTEGER CONSTRAINT NN_SIGNAL_PINSET_PIN_NUMER NOT NULL,
+    comment     TEXT,
+    CONSTRAINT PK_PACKAGE_SIGNAL PRIMARY KEY (
+        pinset_id,
+        signal_id
+    )
+    ON CONFLICT FAIL,
+    CONSTRAINT UQ_SIGNAL_SET_PK_INDEX UNIQUE (
+        pinset_id,
+        signal_id,
+        idx ASC
+    )
+    ON CONFLICT ROLLBACK
+)
+WITHOUT ROWID,
+STRICT;
