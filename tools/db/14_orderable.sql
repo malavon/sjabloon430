81000fa352ddbdc96638dd41be6ec3ec68545470
Benny Goemans <git@malavon.eu>
2024-12-28 03:10:11 +0100

    DB: again changes ... min/max op temp for package, not included signal/pinset etc
    TODO: isn't this the commit (at least after splitting the files) in which orderable became a link table as well between device & package? IMPORTANT to note

--- tools/db/14_orderable.sql
+++ tools/db/14_orderable.sql
@@ -6,13 +6,24 @@
 
 -- Table: orderable
 CREATE TABLE IF NOT EXISTS orderable (
-    name       TEXT    CONSTRAINT UQ_ORDERABLE_NAME UNIQUE ON CONFLICT ROLLBACK
-                       CONSTRAINT NN_ORDERABLE_NAME NOT NULL
-                       PRIMARY KEY,
-    package_id INTEGER CONSTRAINT NN_ORDERABLE_PACKAGE_ID NOT NULL
-                       CONSTRAINT FK_ORDERABLE_PACKAGE REFERENCES package (id) ON DELETE RESTRICT,
-    status     TEXT    NOT NULL,
-    msl_level  INTEGER,
-    comment    TEXT
+    name        TEXT    CONSTRAINT UQ_ORDERABLE_NAME UNIQUE ON CONFLICT ROLLBACK
+                        CONSTRAINT NN_ORDERABLE_NAME NOT NULL
+                        PRIMARY KEY,
+    device_id   INTEGER CONSTRAINT FK_ORDERABLE_DEVICE REFERENCES device (id) ON DELETE RESTRICT
+                        CONSTRAINT NN_ORDERABLE_DEVICE NOT NULL,
+    drawing     TEXT    CONSTRAINT NN_ODBL_DRAWING NOT NULL,
+    pins        INTEGER CONSTRAINT NN_ODBL_PINS NOT NULL,
+    status      TEXT    NOT NULL,
+    msl_level   INTEGER,
+    op_temp_min INTEGER,
+    op_temp_max INTEGER,
+    comment     TEXT,
+    CONSTRAINT FK_ODBL_PACKAGE FOREIGN KEY (
+        drawing,
+        pins
+    )
+    REFERENCES package (drawing,
+    pins) MATCH [FULL]
 )
+WITHOUT ROWID,
 STRICT;
