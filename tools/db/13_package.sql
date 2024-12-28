c0f1bc803b43e2b9167d771a92a2eddfe762c246
Benny Goemans <git@malavon.eu>
2024-12-28 03:10:11 +0100

    DB: again changes ... min/max op temp for package, not included signal/pinset etc
    TODO: isn't this the commit (at least after splitting the files) in which orderable became a link table as well between device & package? IMPORTANT to note

--- tools/db/13_package.sql
+++ tools/db/13_package.sql
@@ -6,18 +6,15 @@
 
 -- Table: package
 CREATE TABLE IF NOT EXISTS package (
-    id        INTEGER PRIMARY KEY ON CONFLICT ROLLBACK AUTOINCREMENT
-                      NOT NULL,
-    device_id INTEGER REFERENCES device (id) ON DELETE CASCADE,
-    type      TEXT    NOT NULL,
-    drawing   TEXT    NOT NULL,
-    pins      INTEGER NOT NULL,
-    comment   TEXT,
-    CONSTRAINT UQ_PACKAGE_DEV_DRW_PINS UNIQUE (
-        device_id,
-        drawing,
-        pins
+    drawing TEXT    NOT NULL,
+    pins    INTEGER NOT NULL,
+    type    TEXT    NOT NULL,
+    comment TEXT,
+    CONSTRAINT PK_PACKAGE_DRW_PINS PRIMARY KEY (
+        drawing ASC,
+        pins ASC
     )
     ON CONFLICT ROLLBACK
 )
+WITHOUT ROWID,
 STRICT;
