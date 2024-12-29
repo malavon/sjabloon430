bc50cffab8327bbb2496c1810fdb32c86f2d5642
Benny Goemans <git@malavon.eu>
2024-12-30 00:28:33 +0100

    cccurses: some empty layout classes to have a general idea

---
+++ tools/pinout/ccurses/layout/absolute_layout.hpp
@@ -0,0 +1,8 @@
+#ifndef CCURSES_LAYOUT_ABSOLUTE_LAYOUT_HPP
+#define CCURSES_LAYOUT_ABSOLUTE_LAYOUT_HPP
+
+namespace ccurses { namespace layout {
+class AbsoluteLayout { };
+}}
+
+#endif // ABSOLUTE-LAYOUT_HPP
