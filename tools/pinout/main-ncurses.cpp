4d3708f3a150f35a2b086fdf13f4c48ff4b8f072
Benny Goemans <git@malavon.eu>
2024-12-29 19:46:25 +0100

    DB Tools Pinout: ncurses might actually be the best choice for a TUI
    I've tried a few TUI libraries: (N)Curses, FTXUI & NotCurses. They're all nice
    libraries with their own focus, however ...
    
    Regarding FTXUI: It seems that there's a policy of learning primarily from
    examples. I guess the manpage-style docs for (n)curses seem to work better for
    me. Then again, if I had been on Windows or something else without man-support,
    I might have made a different choice.
    Maybe I don't really click with FTXUI, I don't know.
    
    One note: FTXUI 5.0.0 seems to have an issue with the FreeBSD virtual terminals
    (or the font used by default) which renders checkboxes inoperable. I'm not sure
    if this can be remedied with options.
    
    NotCurses seems to be well-documented, but my tests in a VT shows that it
    simply doesn't work somehow. I'll be darned ... I wasn't really expecting this,
    but I couldn't get any of the examples working in the VT. X11-based terminals
    work fine, but that's not what I'm looking for when using a TUI library...
    
    Simply put, I'll be learning a bit about ncurses I guess. I chose to write
    some C++ wrappers around the library, even though I'm far from the first.
    Other wrappers aren't very wel documented either and writing wrappers myself
    should mean I get to actually understand curses better.

---
+++ tools/pinout/main-ncurses.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+
+#include "sqlite3.h"
+#include "cccurses/cccurses.hpp"
+#include "cccurses/form.hpp"
+#include "cccurses/window.hpp"
+
+using namespace cccurses;
+using namespace std;
+
+void autofillDatasheet() { }
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	/* Initialize curses */
+	initCurses();
+
+	static const string WIDEST("MSP430F6459-HIREL");
+	static const int MAX_WIDTH = WIDEST.length() + 2;
+
+	{
+		Window top(0, 0, COLS - MAX_WIDTH, 5);
+		Window other(0, 5, COLS - MAX_WIDTH, 10);
+		// base on select max(length(name) from device?
+		Window right(COLS - MAX_WIDTH, 0, MAX_WIDTH, 0);
+
+		top.setTitle("Search");
+		top.addText(1, 3, "HELLO\n");
+		top.addText(2, 3, "HELLO OTHER\n");
+		// whline(top.raw(), '-', 30);
+
+		right.setTitle("Set: default");
+		right.addText(1, 1, WIDEST);
+		right.addText(2, 2, "PZ100");
+
+		top.paint();
+		other.paint();
+		right.paint();
+
+		Form form(top);
+
+		form.addField(1, 20, 1, 20);
+		form.addField(1, 20, 2, 20);
+
+		Field datasheet(1, 20, 1, 20);
+		Field device(1, 20, 2, 20);
+
+		FIELD *field[3];
+		FORM *my_form;
+		int ch;
+
+		/* Initialize few color pairs */
+		init_pair(1, COLOR_WHITE, COLOR_BLUE);
+		init_pair(2, COLOR_WHITE, COLOR_BLUE);
+
+		/* Initialize the fields */
+		// field[0] = new_field(1, 20, 1, 20, 0, 0);
+		// field[1] = new_field(1, 20, 2, 20, 0, 0);
+		// field[2] = NULL;
+		field[0] = NULL;
+
+		/* Set field options */
+		// set_field_fore(field[0], COLOR_PAIR(1)); /* Put the field with blue background */
+		// set_field_back(field[0], COLOR_PAIR(2)); /* and white foreground (characters */
+		// /* are printed in white 	*/
+		// field_opts_off(field[0], O_AUTOSKIP); /* Don't go to next field when this */
+		// /* Field is filled up 		*/
+		// set_field_back(field[1], A_UNDERLINE);
+		// field_opts_off(field[1], O_AUTOSKIP | O_DYNAMIC_JUSTIFY);
+
+		// set_field_type(field[0], TYPE_ALNUM);
+		// set_field_type(field[1], TYPE_INTEGER);
+
+		/* Create the form and post it */
+		my_form = new_form(field);
+		post_form(my_form);
+		refresh();
+
+		// set_current_field(my_form, field[0]); /* Set focus to the colored field */
+		// mvprintw(1, 2, "Datasheet");
+		// mvprintw(2, 2, "Device");
+		// mvprintw(3, 2, "Package Drawings");
+		// mvprintw(LINES - 2, 0, "Use UP, DOWN arrow keys OR TAB/BTAB to switch between fields");
+		// refresh();
+
+		/* Loop through to get user requests */
+		form.loop();
+	}
+
+	/* Un post form and free the memory */
+	// unpost_form(my_form);
+	// free_form(my_form);
+	// free_field(field[0]);
+	// free_field(field[1]);
+
+	endwin(); // important: restores terminal
+
+	return EXIT_SUCCESS;
+}
