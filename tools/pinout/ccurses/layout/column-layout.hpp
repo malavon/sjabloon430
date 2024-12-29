aff880662349505f05380a259169e37dab48d046
Benny Goemans <git@malavon.eu>
2024-12-30 00:28:33 +0100

    cccurses: some empty layout classes to have a general idea

---
+++ tools/pinout/ccurses/layout/column-layout.hpp
@@ -0,0 +1,7 @@
+#ifndef CCURSES_LAYOUT_COLUMN_LAYOUT_HPP
+#define CCURSES_LAYOUT_COLUMN_LAYOUT_HPP
+
+namespace ccurses { namespace layout {
+class ColumnLayout { };
+}}
+#endif // COLUMN-LAYOUT_HPP
---
+++ tools/pinout/ccurses/layout/layout.hpp
@@ -0,0 +1,8 @@
+#ifndef CCURSES_LAYOUT_LAYOUT_HPP
+#define CCURSES_LAYOUT_LAYOUT_HPP
+
+namespace ccurses { namespace layout {
+class Layout { };
+}}
+
+#endif // LAYOUT_HPP
