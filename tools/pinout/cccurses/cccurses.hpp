44c0e7ec44866055cf9c0790885fa4339da83fd9
Benny Goemans <git@malavon.eu>
2024-12-30 00:22:02 +0100

    cccurses: C++ wrapper for ncurses, extremely simple initial version

---
+++ tools/pinout/cccurses/cccurses.hpp
@@ -0,0 +1,30 @@
+#ifndef CCURSES_HPP
+#define CCURSES_HPP
+
+#include "ncurses.h"
+#include "window.hpp"
+
+namespace cccurses {
+
+/*
+ * Temporary init function, without any choice basically. It's all I need right now.
+ * */
+void initCurses() {
+	initscr();
+	start_color();
+	cbreak(); // do buffer input in full-line chunks ended with Enter
+	noecho(); // do not echo characters to terminal immediately
+	intrflush(stdscr, FALSE);
+	keypad(stdscr, TRUE); // enable more keys like the numpad
+
+	// why? no window will be painted unless this is exectured before calling wrefresh() in Window::paint();
+	refresh();
+}
+
+// class Window standard; // TODO: window from stdscr ptr
+
+// TODO: add SIGWINCH handler?
+
+} // namespace ccurses
+
+#endif // CCURSES_HPP
