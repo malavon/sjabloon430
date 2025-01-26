7942dfc31e1e75dda039ba8ce0c2d0b11e731e9f
Benny Goemans <git@malavon.eu>
2025-01-26 20:22:53 +0100

    cccurses: headers in CMakeLists.txt (for editor only)
    Apparently QTCreator doesn't consider header files part of a project if they
    aren't listed in CMakeLists.txt as well. I assume this is because it might be
    difficult to separate system & library headers from project headers.

--- tools/pinout/ccurses/layout/column-layout.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef CCURSES_LAYOUT_COLUMN_LAYOUT_HPP
-#define CCURSES_LAYOUT_COLUMN_LAYOUT_HPP
-
-namespace ccurses { namespace layout {
-class ColumnLayout { };
-}}
-#endif // COLUMN-LAYOUT_HPP
--- tools/pinout/ccurses/layout/flow.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CCURSES_LAYOUT_FLOW_LAYOUT_HPP
-#define CCURSES_LAYOUT_FLOW_LAYOUT_HPP
-
-namespace ccurses { namespace layout {
-
-class FlowLayout { };
-}}
-
-#endif // FLOW-LAYOUT_HPP
