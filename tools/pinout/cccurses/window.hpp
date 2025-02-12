47c3da022d519a1ab7fb2935c38af6943f0784e5
Benny Goemans <git@malavon.eu>
2025-02-12 04:20:32 +0100

    cccurses: add single-char handling, consistent line/cols
    Adding a single character is the norm. Ncurses itself also implements its
    string writing as multiple char insertions. It's normal to include this.

--- tools/pinout/cccurses/window.hpp
+++ tools/pinout/cccurses/window.hpp
@@ -14,7 +14,6 @@ using namespace std;
  */
 class Window {
   private:
-	// default constructor: for flex window?
 	Window() { }
 
   public:
@@ -23,10 +22,30 @@ class Window {
 		assert(nullptr != ptr);
 	}
 
+	Window(WINDOW *win) : ptr(win) { }
+
+	Window(const Window &other) : ptr(other.ptr), title(other.title) { }
+
 	~Window() {
 		if ( ptr != nullptr ) {
-			delwin(ptr);
+			delwin(ptr); // might be dangerous with stdscr?
 		}
+	}
+
+	void addCharacter(const char character) {
+		waddch(ptr, character);
+	}
+
+	void addCharacter(const char character, int attrs) {
+		waddch(ptr, character | attrs);
+	}
+
+	void addCharacter(const int line, const int col, const char character) {
+		mvwaddch(ptr, line, col, character);
+	}
+
+	void addCharacter(const int line, const int col, const char character, int attrs) {
+		mvwaddch(ptr, line, col, character | attrs);
 	}
 
 	void addText(const string &text) {
@@ -39,15 +58,27 @@ class Window {
 		wattroff(ptr, attrs);
 	}
 
-	void addText(const int y, const int x, const string &text) {
-		// mvwaddstr(ptr, y, x, text.c_str());
-		mvwaddnstr(ptr, y, x, text.c_str(), text.length());
+	void addText(const int line, const int col, const string &text) {
+		// mvwaddstr(ptr, line, col, text.c_str());
+		mvwaddnstr(ptr, line, col, text.c_str(), text.length());
 	}
 
-	void addText(const int y, const int x, const string &text, int attrs) {
+	void addText(const int line, const int col, const string &text, int attrs) {
 		wattron(ptr, attrs);
-		addText(y, x, text);
+		addText(line, col, text);
 		wattroff(ptr, attrs);
+	}
+
+	void enableAttributes(const int attrs) {
+		wattron(ptr, attrs);
+	}
+
+	void disableAttributes(const int attrs) {
+		wattroff(ptr, attrs);
+	}
+
+	void moveCursor(const int line, const int col) {
+		wmove(ptr, line, col);
 	}
 
 	void setTitle(const string &title) {
