7e355b2a9a9edaa8645e8d8141b1a70b9ee46f4b
Benny Goemans <git@malavon.eu>
2025-02-16 03:59:14 +0100

    DB Tools Pinout: db totals odd but efficient
    New version of DB total counting to reduce complexity of the C++ code by adding
    complexity in the query. Having a single row of counts and a single column for
    each table makes it much easier to to retrieve them later.
    
    This function is the first in which I create the SQLite prepared statement only
    once. This is the better way, since SQLite keeps prepared statements internally
    which in turn means multiple calls to this function would have turned in some
    sort of a memory leak. For those afficionado's of other languages: both Java
    and Rust wouldn't have been to fix this either. Of course I would be surprised
    if a Java-based project like Hibernate would do an internal query-cache, but
    it does make sense to do so actually.
    
    Hindsight: after writing other functions that execute these prepared statements
    it became clear that these 5 lines are duplicated all over this file. Therefore
    I chose to extract the prepare() function.

--- tools/pinout/database.hpp
+++ tools/pinout/database.hpp
@@ -16,7 +16,8 @@ struct DatabaseTotals {
 /* SQLite 3 init & database import/export */
 sqlite3 *createDatabase();
 
-DatabaseTotals retrieveDBCounts(sqlite3 *db);
+/* Query DB */
+DatabaseTotals countTotals(sqlite3 *db);
 
 }}}} // namespace sjabloon430::tools::pinout::db
 #endif // SJABLOON430_TOOLS_PINOUT_DATABASE_HPP
