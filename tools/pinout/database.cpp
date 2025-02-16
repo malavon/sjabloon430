21347fdc46cc8223de2fab2be846f8e1384498c4
Benny Goemans <git@malavon.eu>
2025-02-16 03:34:35 +0100

    DB Tools Pinout: naive version of db totals
    This version - especially with what I know while I'm rebasing this commit and
    expanding the comment - is preliminary while I was searching for an acceptable
    solution. In short, it returns each table count on a separate line in a pretty
    convoluted way.

--- tools/pinout/database.cpp
+++ tools/pinout/database.cpp
@@ -1,5 +1,6 @@
-
 #include "database.hpp"
+
+#include <iostream>
 
 namespace sjabloon430 { namespace tools { namespace pinout { namespace db {
 
@@ -18,6 +19,50 @@ sqlite3 *createDatabase() {
 		sqlite3_exec(db, "PRAGMA foreign_keys = ON", nullptr, nullptr, nullptr);
 	}
 	return db;
+}
+
+DatabaseTotals retrieveDBCounts(sqlite3 *db) {
+	// something tells me there is an easier method of
+	// aggregating these 4 counts in a single result row ...
+	const char *QUERY = "select 1, count(*) from datasheet\
+		union select 2, count(*) from device \
+		union select 3, count(*) from package \
+		union select 4, count(*) from orderable;";
+	// I remember doing at work (Oracle) something with some sort of dummy table
+
+	sqlite3_stmt *stmt;
+	DatabaseTotals totals;
+	totals.datasheets = -1;
+	totals.devices = -1;
+	totals.orderables = -1;
+	totals.packages = -1;
+
+	if ( sqlite3_prepare_v2(db, QUERY, -1, &stmt, NULL) != SQLITE_OK ) {
+		std::cout << "SQLite3 error " << sqlite3_errmsg(db) << std::endl;
+	} else {
+		while ( sqlite3_step(stmt) == SQLITE_ROW ) {
+			int det = sqlite3_column_int(stmt, 0);
+			int count = sqlite3_column_int(stmt, 1);
+			switch ( det ) {
+				case 1:
+					totals.datasheets = count;
+					break;
+				case 2:
+					totals.devices = count;
+					break;
+				case 3:
+					totals.packages = count;
+					break;
+				case 4:
+					totals.orderables = count;
+					break;
+				default:
+					std::cout << "ERROR: SQL result unknown: " << det << std::endl;
+			}
+		}
+	}
+
+	return totals;
 }
 
 }}}} // namespace sjabloon430::tools::pinout::db
