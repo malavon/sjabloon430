db99451059b9ffcf668b284aaeea159e9b87a433
Benny Goemans <git@malavon.eu>
2025-02-16 03:59:14 +0100

    DB Tools Pinout: db totals odd but efficient
    New version of DB total counting to reduce complexity of the C++ code by adding
    complexity in the query. Having a single row of counts and a single column for
    each table makes it much easier to to retrieve them later.
    
    This function is the first in which I create the SQLite prepared statement only
    once. This is the better way, since SQLite keeps prepared statements internally
    which in turn means multiple calls to this function would have turned in some
    sort of a memory leak. For those afficionado's of other languages: both Java
    and Rust wouldn't have been to fix this either. Of course I would be surprised
    if a Java-based project like Hibernate would do an internal query-cache, but
    it does make sense to do so actually.
    
    Hindsight: after writing other functions that execute these prepared statements
    it became clear that these 5 lines are duplicated all over this file. Therefore
    I chose to extract the prepare() function.

--- tools/pinout/database.cpp
+++ tools/pinout/database.cpp
@@ -1,6 +1,8 @@
 #include "database.hpp"
 
+#include <cassert>
 #include <iostream>
+#include <set>
 
 namespace sjabloon430 { namespace tools { namespace pinout { namespace db {
 
@@ -21,35 +23,39 @@ sqlite3 *createDatabase() {
 	return db;
 }
 
-DatabaseTotals retrieveDBCounts(sqlite3 *db) {
-	// something tells me there is an easier method of
-	// aggregating these 4 counts in a single result row ...
-	const char *QUERY = "select * from "
-			    "(	select 'datasheet' as ordr, count(*) from datasheet"
-			    "	union select 'device', count(*) from device"
-			    "	union select 'orderable', count(*) from orderable"
-			    "	union select 'package', count(*) from package"
-			    ") order by ordr asc;";
-	// I remember doing at work (Oracle) something with some sort of dummy table
+/* Query DB */
 
-	sqlite3_stmt *stmt;
-	DatabaseTotals totals = {-1, -1, -1, -1};
+void prepare(sqlite3 *db, sqlite3_stmt **stmt, const char *QUERY) {
+	int rc = sqlite3_prepare_v2(db, QUERY, -1, stmt, NULL);
+	if ( rc != SQLITE_OK ) {
+		std::cerr << "SQLite3 error " << sqlite3_errmsg(db) << std::endl;
+	}
+	assert(rc == SQLITE_OK);
+}
 
-	if ( sqlite3_prepare_v2(db, QUERY, -1, &stmt, NULL) != SQLITE_OK ) {
-		std::cout << "SQLite3 error " << sqlite3_errmsg(db) << std::endl;
-	} else {
-		if ( sqlite3_step(stmt) == SQLITE_ROW ) {
-			totals.datasheets = sqlite3_column_int(stmt, 1);
-		}
-		if ( sqlite3_step(stmt) == SQLITE_ROW ) {
-			totals.devices = sqlite3_column_int(stmt, 1);
-		}
-		if ( sqlite3_step(stmt) == SQLITE_ROW ) {
-			totals.orderables = sqlite3_column_int(stmt, 1);
-		}
-		if ( sqlite3_step(stmt) == SQLITE_ROW ) {
-			totals.packages = sqlite3_column_int(stmt, 1);
-		}
+DatabaseTotals countTotals(sqlite3 *db) {
+	// a single result row is best, 1 column per count
+	// this is the best I can do with my knowledge of sqlite
+	static const char *QUERY = "SELECT SUM(datasheets), SUM(devices), SUM(orderables), SUM(packages) "
+				   "FROM ("
+				   "	SELECT 0 AS datasheets, 0 AS devices, 0 AS orderables, 0 AS packages"
+				   "	UNION SELECT COUNT(*), 0, 0, 0 FROM datasheet"
+				   "	UNION SELECT 0, COUNT(*), 0, 0 FROM device"
+				   "	UNION SELECT 0, 0, COUNT(*), 0 FROM orderable"
+				   "	UNION SELECT 0, 0, 0, COUNT(*) FROM package )";
+
+	static sqlite3_stmt *stmt;
+	if ( stmt == nullptr ) {
+		prepare(db, &stmt, QUERY);
+	}
+
+	DatabaseTotals totals{-1, -1, -1, -1};
+
+	if ( sqlite3_step(stmt) == SQLITE_ROW ) {
+		totals.datasheets = sqlite3_column_int(stmt, 0);
+		totals.devices = sqlite3_column_int(stmt, 1);
+		totals.orderables = sqlite3_column_int(stmt, 2);
+		totals.packages = sqlite3_column_int(stmt, 3);
 	}
 
 	return totals;
