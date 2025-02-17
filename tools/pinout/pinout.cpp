b8c254e2afa60522b4c1c60e582f5fac982d4181
Benny Goemans <git@malavon.eu>
2025-02-17 03:51:05 +0100

    DB Tools Pinout: cccurses' border calculation changes incorporated

--- tools/pinout/pinout.cpp
+++ tools/pinout/pinout.cpp
@@ -17,6 +17,7 @@ void printShortcuts();
 
 static const int WIDEST_MODEL_LENGTH = strlen("MSP430F6459-HIREL"); /* hardcoded longest model */
 static const int WIN_TOP_HEIGHT = 5;
+static const int WIN_CONFIGSET_WIDTH = WIDEST_MODEL_LENGTH + 2 /* indent */ + 2 /* borders */;
 
 int main() {
 	setlocale(LC_ALL, "");
@@ -31,20 +32,16 @@ int main() {
 	/* Initialize curses */
 	initCurses();
 
-	static const int MODEL_INDENT = 1;
-	// base on select max(length(name)) + MODEL_INDENT + 2 from device?
-	static const int MAX_WIDTH = WIDEST_MODEL_LENGTH + MODEL_INDENT + 2 /*border*/;
-
 	{
-		Window top(WIN_TOP_HEIGHT, COLS - MAX_WIDTH, 0, 0);
+		Window top(WIN_TOP_HEIGHT, COLS - WIN_CONFIGSET_WIDTH, 0, 0);
 
-		Window defaultSet(10, MAX_WIDTH, 0, COLS - MAX_WIDTH);
-		Window newSet(3, MAX_WIDTH, 10, COLS - MAX_WIDTH);
+		Window defaultSet(10, WIN_CONFIGSET_WIDTH, 0, COLS - WIN_CONFIGSET_WIDTH);
+		Window newSet(3, WIN_CONFIGSET_WIDTH, 10, COLS - WIN_CONFIGSET_WIDTH);
 		// TODO: no border, separate with hline or something?
-		Window firstPin(6, COLS - MAX_WIDTH, 6, 0);
-		Window secondPin(5, COLS - MAX_WIDTH, 12, 0);
+		Window firstPin(WIN_TOP_HEIGHT + 1, COLS - WIN_CONFIGSET_WIDTH, WIN_TOP_HEIGHT + 1, 0);
+		Window secondPin(5, COLS - WIN_CONFIGSET_WIDTH, 12, 0);
 
-		int lr = 1;
+		int lr = 0;
 		defaultSet.setTitle("Default");
 		defaultSet.add(lr++, 1, "Models: MOCK UP!");
 		defaultSet.add(lr++, 2, "MSP430FR2532");
@@ -58,14 +55,14 @@ int main() {
 		newSet.add(1, 1, "F5: Create new set");
 
 		// note: 3 characters for pin number is enough (even for BGA)
-		firstPin.add(1, 1, " RHB32   DA32  RGE24  YQW24\tSignal\t\tDescription");
-		firstPin.add(2, 1, "     1      5      1     E1\t~RST\t\tActive-low reset input");
-		firstPin.add(3, 1, "                           \tNMI\t\tNonmaskable interrupt input");
-		firstPin.add(4, 1, "                           \tSBWTDIO\t\tSpy-Bi-Wire data input/output");
+		firstPin.add(0, 1, " RHB32   DA32  RGE24  YQW24\tSignal\t\tDescription");
+		firstPin.add(1, 1, "     1      5      1     E1\t~RST\t\tActive-low reset input");
+		firstPin.add(2, 1, "                           \tNMI\t\tNonmaskable interrupt input");
+		firstPin.add(3, 1, "                           \tSBWTDIO\t\tSpy-Bi-Wire data input/output");
 
-		secondPin.add(1, 1, " RHB32   DA32  RGE24  YQW24\tSignal\t\tDescription");
-		secondPin.add(2, 1, "     2      6      2     D2\tTEST\t\tTest Mode pin");
-		secondPin.add(3, 1, "                           \tSBWTCK\t\tSpy-Bi-Wire input clock ");
+		secondPin.add(0, 1, " RHB32   DA32  RGE24  YQW24\tSignal\t\tDescription");
+		secondPin.add(1, 1, "     2      6      2     D2\tTEST\t\tTest Mode pin");
+		secondPin.add(2, 1, "                           \tSBWTCK\t\tSpy-Bi-Wire input clock ");
 
 		printShortcuts();
 
@@ -84,13 +81,15 @@ int main() {
 		});
 		db::DatabaseTotals totals = db::countTotals(db);
 
+		int topLine = 0;
+		int topCol = 1;
 		top.setTitle("Search");
-		top.add(1, 3, "Datasheet: SLAS942\t(c) 11/2015");
-		top.add(2, 3, "Revision: E\t\t(c) 12/2019");
+		top.add(topLine + 0, topCol, "Datasheet: SLAS942\t(c) 11/2015");
+		top.add(topLine + 1, topCol, "Revision:  E\t\t(c) 12/2019");
 		// currently doesn't work
 		// top.print(4, 1, "DB contains: %i datasheets, %i devices, %d orderables, %i packages", totals.datasheets, totals.devices, totals.orderables,
 		// totals.packages);
-		top.add(3, 3, "DB contains: ");
+		top.add(topLine + 2, topCol, "DB contains: ");
 		top.add(std::to_string(totals.datasheets));
 		top.add(" datasheets, ");
 		top.add(std::to_string(totals.devices));
@@ -195,27 +194,31 @@ void printShortcuts() {
 	printShortcut(hotkeyWin, "F5-F9", "Set #");
 }
 
-// open a window in middle of screen, allow searching database
+// open a window in middle of screen, allow searching database; mock-up
 void searchDatasheet(sqlite3 *db) {
-	static const string PROMPT("Search by datasheet or device");
+	// TODO: also add some help on this window/form fields using F1
 	static const string DATASHEET_HDR("Datasheet ");
 	static const string MODEL_HDR("Model ");
-	const int MAX_WIDTH = max(PROMPT.length() + 2 + 2 /* last +2 = border */,
-				  DATASHEET_HDR.length() + (WIDEST_MODEL_LENGTH + 1) * 2 + 2 /* last +2 = border */);
+	static const int MAX_WIDTH =
+	    1 + DATASHEET_HDR.length() + (WIDEST_MODEL_LENGTH + 1) * 2 + 1 /* space */ + 2 /* border */;
+	static const int MAX_HEIGHT = 6 + 2 /* border */;
 
-	const int MAX_HEIGHT = 8 + 2; // last +2 = border
 	// TODO: doesn't care about resizing or too small a screen
 	Window center(MAX_HEIGHT, MAX_WIDTH, (LINES - MAX_HEIGHT) / 2, (COLS - MAX_WIDTH) / 2);
+	center.setTitle("Search");
 
-	// still takes into account (internal) border until ccurses takes this into account
-	int line = 1;
-	center.add(line, 2, PROMPT);
-	center.add(++ ++line, 2, DATASHEET_HDR);
-	center.add(line, 12, "SLAS555", WA_STANDOUT);
-	center.add(++ ++line, 2, MODEL_HDR);
-	center.add(line, 12, "MSP430F6459-HIREL", WA_STANDOUT);
-	center.add(' ');
-	center.add("MSP430F6459-HIREL");
+	const int line = 1;
+	const int hdrCol = 1;
+	const int fieldCol = hdrCol + DATASHEET_HDR.length();
+	const int menuCol = fieldCol + WIDEST_MODEL_LENGTH + 2;
+	center.add(line + 0, hdrCol, DATASHEET_HDR);
+	center.add(line + 0, fieldCol, "SLAS942", WA_STANDOUT);
+	center.add(line + 2, hdrCol, MODEL_HDR);
+	center.add(line + 2, fieldCol, "MSP430F645       ", WA_STANDOUT);
+	center.add(line + 0, menuCol - 1, '>');
+	center.add(line + 0, menuCol, "MSP430F6458");
+	center.add(line + 1, menuCol, "MSP430F6459");
+	center.add(line + 2, menuCol, "MSP430F6459-HIREL");
 
 	static const string BUTTON_TEXT = "[OPEN]";
 	center.add(MAX_HEIGHT - 3, (MAX_WIDTH - BUTTON_TEXT.length()) / 2, BUTTON_TEXT, COLOR_PAIR(3));
