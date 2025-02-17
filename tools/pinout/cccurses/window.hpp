1beb9b1a89a737390b3a2559f4ad26b23a90326f
Benny Goemans <git@malavon.eu>
2025-02-17 03:29:18 +0100

    cccurses: inner window to accomodate border and fix wrapping
    TODO explanation
    TODO rebase out currently unused border & title classes?

--- tools/pinout/cccurses/window.hpp
+++ tools/pinout/cccurses/window.hpp
@@ -8,6 +8,16 @@
 namespace cccurses {
 using namespace std;
 
+namespace inner {
+class Border { };
+
+class BorderTitle {
+  private:
+	string title;
+	char pre, post;
+};
+}
+
 /**
  * @brief The Window class is a very simple wrapper around curses WINDOW. It's far from useful in all cases right now.
  */
@@ -18,61 +28,66 @@ class Window {
   public:
 	Window(unsigned int line, unsigned int col, unsigned int height, unsigned int width) {
 		ptr = newwin(height, width, line, col);
+		inner = subwin(ptr, height - 2, width - 2, line + 1, col + 1);
 	}
-
-	Window(WINDOW *win) : ptr(win) { }
 
 	Window(const Window &other) : ptr(other.ptr), title(other.title) { }
 
+	// shouldn't really be here, it only exists for wrapping stdscr
+	Window(WINDOW *win) : ptr(win), inner(win) { }
+
 	~Window() {
+		if ( inner != nullptr ) {
+			delwin(ptr);
+		}
 		if ( ptr != nullptr ) {
 			delwin(ptr); // might be dangerous with stdscr?
 		}
 	}
 
 	void add(const char character) {
-		waddch(ptr, character);
+		waddch(inner, character);
 	}
 
 	void add(const int raw) {
-		waddch(ptr, raw);
+		waddch(inner, raw);
 	}
 
 	void add(const string &text) {
-		waddnstr(ptr, text.c_str(), text.length());
+		waddnstr(inner, text.c_str(), text.length());
 	}
 
 	void add(const char character, int attrs) {
-		waddch(ptr, character | attrs);
+		waddch(inner, character | attrs);
 	}
 
 	void add(const string &text, int attrs) {
-		wattron(ptr, attrs);
+		wattron(inner, attrs);
 		add(text);
-		wattroff(ptr, attrs);
+		wattroff(inner, attrs);
 	}
 
 	void add(const int line, const int col, const char character) {
-		mvwaddch(ptr, line, col, character);
+		mvwaddch(inner, line, col, character);
 	}
 
 	void add(const int line, const int col, const int raw) {
-		mvwaddch(ptr, line, col, raw);
+		mvwaddch(inner, line, col, raw);
 	}
 
 	void add(const int line, const int col, const string &text) {
-		// mvwaddstr(ptr, line, col, text.c_str());
-		mvwaddnstr(ptr, line, col, text.c_str(), text.length());
+		// mvwaddstr(inner, line, col, text.c_str());
+		mvwaddnstr(inner, line, col, text.c_str(), text.length());
 	}
 
 	void add(const int line, const int col, const char character, int attrs) {
-		mvwaddch(ptr, line, col, character | attrs);
+		mvwaddch(inner, line, col, character | attrs);
 	}
 
 	void add(const int line, const int col, const string &text, int attrs) {
-		wattron(ptr, attrs);
+		wattron(inner, attrs);
 		add(line, col, text);
-		wattroff(ptr, attrs);
+		wattroff(inner, attrs);
 	}
 
 	void enableAttributes(const int attrs) {
@@ -84,7 +99,7 @@ class Window {
 	}
 
 	void moveCursor(const int line, const int col) {
-		wmove(ptr, line, col);
+		wmove(inner, line, col);
 	}
 
 	// these don't work, need to pass the variadic argument somehow
@@ -107,6 +122,8 @@ class Window {
 		wborder(ptr, 0, 0, 0, 0, 0, 0, 0, 0);
 		mvwaddnstr(ptr, 0, 2, title.c_str(), title.length());
 		wrefresh(ptr);
+		touchwin(ptr);
+		wrefresh(inner);
 	}
 
 	// TEMPORARY?
@@ -115,7 +132,11 @@ class Window {
 	}
 
   private:
+	/* Window pointer, including border */
 	WINDOW *ptr;
+	/* Inner window, inside border */
+	WINDOW *inner;
+
 	string title;
 
 	friend class Form;
