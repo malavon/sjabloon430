0f4d8f4fe010c0f05482d75ab33d5da5e47c7728
Benny Goemans <git@malavon.eu>
2025-02-18 06:47:00 +0100

    ccurses: ccurses.hpp should be included only once, made this possible
    A few header files should be included only once. These are now only included
    from ccurses.hpp, which is the include-once main file.

---
+++ tools/pinout/cccurses/color-internal.hpp
@@ -0,0 +1,36 @@
+#ifndef CCURSES_COLOR_INTERNAL_HPP
+#define CCURSES_COLOR_INTERNAL_HPP
+
+#include "colors.hpp"
+#include "ncurses.h"
+
+namespace cccurses { namespace internal {
+
+void defineColors() {
+	// TODO: FreeBSD terminals do not allow changing colors? :(
+	if ( has_colors() && can_change_color() ) {
+		init_color(8, 0xed, 0xee, 0x1e);
+	}
+}
+
+void defineColorPairs() {
+	/* Initialize few hard-coded color pairs */
+	init_pair(COLOR_PAIR_FORM_SELECTED, COLOR_BLACK, COLOR_YELLOW);
+	init_pair(COLOR_PAIR_FORM_INVALID, COLOR_WHITE, COLOR_RED);
+	init_pair(COLOR_PAIR_FORM_VALID, COLOR_WHITE, COLOR_BLUE);
+
+	init_pair(COLOR_PAIR_BUTTON_DEFAULT, COLOR_BLACK, COLOR_BLUE);
+	init_pair(COLOR_PAIR_BUTTON_SELECTED, COLOR_WHITE, COLOR_BLUE);
+}
+
+void initColors() {
+	if ( has_colors() ) {
+		start_color();
+	}
+	defineColors();
+	defineColorPairs();
+}
+
+}} // namespace cccurses::internal
+
+#endif // CCURSES_COLOR_INTERNAL_HPP
