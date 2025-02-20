739bcccc2f69cffe4e1fa77b7a29089b18cd163c
Benny Goemans <git@malavon.eu>
2025-02-20 03:25:41 +0100

    DB Tools Pinout: first part of pin editing/insertion FAR FROM DONE

--- tools/pinout/database.hpp
+++ tools/pinout/database.hpp
@@ -32,6 +32,8 @@ void importDatabase(sqlite3 *db, const filesystem::path &dbDir, function<void(co
 
 /* Query DB */
 Datasheet findDatasheet(sqlite3 *db, const string id);
+vector<string> findModelsByDatasheet(sqlite3 *db, const string datasheetId);
+vector<string> findPackagesByDatasheet(sqlite3 *db, const string datasheetId);
 unordered_map<string, string> listAllModelsAndDatasheets(sqlite3 *db);
 
 DatabaseTotals countTotals(sqlite3 *db);
