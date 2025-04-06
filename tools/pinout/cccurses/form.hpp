c06b3303f5dd84c810a05cd65eaf1d12c15cdc9c
Benny Goemans <git@malavon.eu>
2025-04-07 01:27:49 +0200

    DB Tools Pinout: PinSet view (temporary?) modifications and updates to callback
    TODO add explanation or rebase

--- tools/pinout/cccurses/form.hpp
+++ tools/pinout/cccurses/form.hpp
@@ -170,7 +170,7 @@ class FormEventHandlerWrapper {
 };
 }
 
-using namespace cccurses::internal;
+using namespace internal;
 
 class SimpleFormKeyEventConsumer : public KeyEventConsumer<FORM *> {
   public:
