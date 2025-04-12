060e71ca133b79d36acc6471df68e12a466957e3
Benny Goemans <git@malavon.eu>
2025-04-12 02:46:30 +0200

    DB Tools Pinout: keeping pins & signals in-memory + visualization
    While there is still no actual usefulness to the program, this version at least
    gives some indication of how the tool is supposed to operate.
    Each pin/signal assignment is separated from the next one with a horizontal
    line. In the future it would probably be better to print the packages more
    frequently, this version only prints it once at the top of the list and once
    on top of the editing pin.
    
    Earlier I saw a window clear on each form repost, which was annoying. After
    investigation this seems to be caused in the pinout code itself: the sub-window
    of the form which was created always overlapped the first 10 lines of the main
    window. This gave the perception that the entire window was cleared, but most
    likely it were only the first 10 lines.

--- tools/pinout/database.hpp
+++ tools/pinout/database.hpp
@@ -38,6 +38,7 @@ Datasheet findDatasheet(sqlite3 *db, const string id);
 vector<string> findModelsByDatasheet(sqlite3 *db, const string datasheetId);
 vector<string> findPackagesByDatasheet(sqlite3 *db, const string datasheetId);
 unordered_map<string, string> listAllModelsAndDatasheets(sqlite3 *db);
+unordered_map<string, string> listAllSignalDescriptions(sqlite3 *db);
 
 }}}} // namespace sjabloon430::tools::pinout::db
 #endif // SJABLOON430_TOOLS_PINOUT_DATABASE_HPP
--- tools/pinout/ui.hpp
+++ tools/pinout/ui.hpp
@@ -14,12 +14,15 @@ using namespace sjabloon430::tools::pinout::db;
 
 // 3 characters is enough for pin numbers, even BGA
 // using 4 is however logical, esthetic purposes 1 empty character always
-static const int PIN_FIELD_WIDTH = 4;
-// packages (drawing + pins) are up to 6 wide, so always format them at 6
-static const int PKG_HDR_WIDTH = 6;
+static const int FIELD_WIDTH_PIN = 4;
 // signal is max ... TODO=
 static const int FIELD_WIDTH_SIGNAL = 8;
 static const int FIELD_WIDTH_DESC = 20;
+// packages (drawing + pins) are up to 6 wide, so always format them at 6
+static const int HEADER_WIDTH_PKG = 6;
+
+// hard-coded max # of signals required for window size
+static const int MAX_SIGNALS = 10;
 
 static const char *SIGNAL_HDR("SIGNAL");
 static const char *DESCRIPTION_HDR("DESCRIPTION");
@@ -34,13 +37,10 @@ struct PinView {
 struct PinSetView {
 	// fixed list of packages
 	const vector<string> pkgs;
-	// semi-fixed list of all existing signals
-	// new signals can be added
-	// TODO: set that doesn't allow deletion better? also vector not really best?
-	// maybe better map type?
-	vector<struct SignalView> signals;
+	// map of all existing signals & descriptions, can be modified (well, extended at least)!
+	unordered_map<string, string> signalDescs;
 	// each item on the screen
-	vector<PinView> pins;
+	vector<PinView> pinViews;
 	/*
 	 * index of pin that is edited
 	 * if higher than pins.size(), add at end
@@ -49,16 +49,11 @@ struct PinSetView {
 	int editIdx = -1;
 };
 
-struct SignalView {
-	string signal;
-	string desc;
-};
-
 // partial drawing functions
 // TODO: window should scroll working
 void drawPinSetHeader(Window &, const int row, const vector<string> &pkgs);
-void drawPinSet(Window &, int row, const vector<string> &pkgs, PinView &pv);
-void editPinSet(Window &, int row, const vector<string> &pkgs, PinView &pv);
+void drawPinSet(Window &, int &row, const vector<string> &pkgs, unordered_map<string, string> &signals, const PinView &pv);
+void editPinSet(Window &, int &row, const vector<string> &pkgs, unordered_map<string, string> &signals, PinView &pv);
 
 // Window drawing functions
 void drawPinSetEditingWindow(Window &win, PinSetView &pinView);
