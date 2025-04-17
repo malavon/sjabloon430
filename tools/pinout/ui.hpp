c663d8f53a894cd3438546b27e3dc0792450b203
Benny Goemans <git@malavon.eu>
2025-04-17 03:38:12 +0200

    DB Tools Pinout: rendering (default) set window for selected datasheet
    Previously the (default) set window was mocked, but now the correct models and
    packages are shown.
    The next set is still mocked, since there is no logic (nor key-handling or
    seperate window) implemented yet.
    to create new sets

--- tools/pinout/ui.hpp
+++ tools/pinout/ui.hpp
@@ -46,6 +46,7 @@ struct SignalView {
 // TODO: edit/view
 // TODO: window should scroll working
 void drawPinSet(Window &win, PinSetView pinView);
+void drawSetConfigWindow(BorderedWindow &, const vector<string> &models, const vector<string> &packages);
 void drawTopWindow(BorderedWindow &, const Datasheet &, DatabaseTotals &);
 string searchDatasheet(const unordered_map<string, string> &dsModels, const int widestModelLength);
 
