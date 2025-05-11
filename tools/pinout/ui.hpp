413bab84025027d07868dd43cf57942eea897262
Benny Goemans <git@malavon.eu>
2025-05-11 03:23:51 +0200

    DB Tools Pinout: reordering packages feature
    Having the possibility to reorder packages in the same order as they are
    mentioned in the datasheet is required for sensible error-proof pinout editing.
    The current version is implemented with regular ncurses wgetch() calls and case
    switching. I'm not convinced that the current cccurses input handling will
    result in considerably shorter code. It would however move some of the code
    outside of this function, which may or may not confuse readers.

--- tools/pinout/ui.hpp
+++ tools/pinout/ui.hpp
@@ -59,6 +59,7 @@ void editPinSet(Window &, int &row, const vector<string> &pkgs, unordered_map<st
 void drawPinSetEditingWindow(Window &win, PinSetView &pinView);
 void drawSetConfigWindow(BorderedWindow &, const vector<string> &models, const vector<string> &packages);
 void drawTopWindow(BorderedWindow &, const Datasheet &, DatabaseTotals &);
+void reorderPackages(vector<string> &pkgs);
 string searchDatasheet(const unordered_map<string, string> &dsModels, const int widestModelLength);
 
 }}}} // namespace sjabloon430::tools::pinout::ui
