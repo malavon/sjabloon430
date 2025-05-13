0539a6d93595d07a5e8bb0caaec7d94049936d14
Benny Goemans <git@malavon.eu>
2025-05-13 04:03:27 +0200

    DB Tools Pinout: export signals to SQL file, including signalgroup data
    Signals are exported into a single export file (24_signal.sql) because they're
    reused in different datasheets/for different models.
    
    Signalgroups are also present in 24_signal.sql and need to be exported as well.
    They're not editable in the pinout application though.

--- tools/pinout/database-files.hpp
+++ tools/pinout/database-files.hpp
@@ -39,8 +39,9 @@ struct ExportConfig {
 	}
 };
 
-// export from a simple query, cannot export joined tables etc
-void exportFromPrepStmt(sqlite3_stmt *statement, const string fileName, const ExportConfig &config = ExportConfig{});
+// only publicly-accessible functions are listed here; privately used ones are in the .cpp file
+
+void exportSignals(sqlite3 *db);
 void importDatabase(sqlite3 *db, std::function<void(const std::string &file, const char *error)> callback);
 
 }}} // namespace sjabloon430::tools::db
