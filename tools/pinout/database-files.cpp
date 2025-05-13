c9be24e0df6719fc74665365376e35e5748487f6
Benny Goemans <git@malavon.eu>
2025-05-13 04:03:27 +0200

    DB Tools Pinout: general SQL export to persist in-memory database
    Exporting to SQL files is the best way to save a SQLite database for this
    project. It's the reason I chose an in-memory database, because there is no
    non-binary file format in SQLite. In fact, it surprises me that there is no
    export to SQL function directly in SQLite itself.
    
    The provided function exports a single prepared statement. The reason I chose
    this above exporting a single table is that I want to create SQL files for each
    datasheet id (e.g. SLAS555) instead of a single big export. This makes it
    easier both to add and to check which pinouts have been already created.

--- tools/pinout/database-files.cpp
+++ tools/pinout/database-files.cpp
@@ -15,6 +15,96 @@ using namespace std::filesystem;
 
 namespace sjabloon430 { namespace tools { namespace db {
 
+enum class ColumnDecl { NULL_VALUE, NUMBER, TEXT };
+
+/* export helper functions */
+string inferColumnName(sqlite3_stmt *stmt, int idx) {
+	const char *origin = sqlite3_column_origin_name(stmt, idx); // column name from DB
+	if ( origin == nullptr ) {
+		const char *col = sqlite3_column_name(stmt, idx); // column name from AS-statement
+		return col == nullptr ? "ERROR_NO_COL_NAME" : col;
+	} else {
+		return origin;
+	}
+}
+
+ColumnDecl inferColumnDecl(sqlite3_stmt *stmt, int idx) {
+	const char *type = sqlite3_column_decltype(stmt, idx);
+	const unsigned char *value = sqlite3_column_text(stmt, idx);
+	if ( value == nullptr ) {
+		return ColumnDecl::NULL_VALUE;
+	} else if ( strcmp(type, "INTEGER") == 0 || strcmp(type, "REAL") == 0 ) {
+		return ColumnDecl::NUMBER;
+	} else {
+		return ColumnDecl::TEXT;
+	}
+}
+
+string insertStringFromResultSet(sqlite3_stmt *stmt) {
+	const int columns = sqlite3_column_count(stmt);
+	if ( columns == 0 ) {
+		return "ERROR NO COLUMNS";
+	}
+
+	const char *table = sqlite3_column_table_name(stmt, 0);
+	string insert = "INSERT INTO ";
+	insert += table;
+	insert += " (";
+	for ( int c = 0; c < columns; c++ ) {
+		insert += inferColumnName(stmt, c);
+		insert += ", ";
+	}
+	insert.erase(insert.size() - strlen(", ")); // remove last ", "
+	insert += ") VALUES (";
+
+	for ( int c = 0; c < columns; c++ ) {
+		ColumnDecl cd = inferColumnDecl(stmt, c);
+		const char *value = reinterpret_cast<const char *>(sqlite3_column_text(stmt, c));
+		if ( ColumnDecl::NULL_VALUE == cd ) {
+			insert += "NULL";
+		} else if ( ColumnDecl::NUMBER == cd ) {
+			insert += value;
+		} else /* ColumnDecl::Text */ {
+			insert += "'";
+			insert += value;
+			insert += "'";
+		}
+		insert += ", ";
+	}
+	insert.erase(insert.size() - strlen(", ")); // remove last ", "
+	insert += ");";
+
+	return insert;
+}
+
+void exportFromPrepStmt(sqlite3_stmt *stmt, const string fileName, bool append) {
+	std::filesystem::path outFile(DB_DIRECTORY);
+	outFile /= fileName;
+	std::ofstream out(outFile, append ? (ios::out | ios::app) : ios::out); // output & append (todo)
+
+	if ( append ) {
+		out << endl; // todo: some custom comment? table name? difficult
+	} else {
+		out << "--" << endl;
+		out << "-- File generated with one of the sjabloon 430 database tools." << endl;
+		out << "-- Any alterations to this data will be kept by these tools, assuming the SQL is still valid." << endl;
+		out << "-- DO NOT REFORMAT THIS FILE AND CHECK IN ONLY AFTER RE-EXPORTING WITH A SJABLOON 430 TOOL." << endl;
+		out << "--" << endl;
+		out << "-- Text encoding used: UTF-8" << endl;
+		out << "--" << endl;
+	}
+	out << "BEGIN TRANSACTION;" << endl << endl;
+	int rc = sqlite3_step(stmt);
+	while ( SQLITE_ROW == rc ) {
+		out << insertStringFromResultSet(stmt) << endl;
+		if ( SQLITE_DONE == (rc = sqlite3_step(stmt)) ) {
+			out << endl;
+		};
+	}
+	out << endl << "COMMIT TRANSACTION;" << endl;
+	sqlite3_finalize(stmt);
+}
+
 // maybe this should be a function shared with other programs
 void importDatabase(sqlite3 *db, std::function<void(const std::string &file, const char *error)> callback) {
 	std::filesystem::path dbDir(DB_DIRECTORY);
