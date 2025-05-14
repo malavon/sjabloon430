bfe8cdb146ab95ef691fcd46c6a4d1b687f2095f
Benny Goemans <git@malavon.eu>
2025-05-15 01:28:13 +0200

    signal group lookup table included, using feature column as lookup didn't work

--- tools/db/14_signal.sql
+++ tools/db/14_signal.sql
@@ -4,11 +4,30 @@
 -- Text encoding used: UTF-8
 --
 
+-- Table: signalgroup
+CREATE TABLE IF NOT EXISTS signalgroup (
+    name TEXT CONSTRAINT PK_SIGNALGROUP_NAME PRIMARY KEY
+            CONSTRAINT UQ_SIGNALGROUP_NAME UNIQUE
+);
+
+BEGIN TRANSACTION;
+
+INSERT INTO signalgroup (name) VALUES ('ADC_SAR');
+INSERT INTO signalgroup (name) VALUES ('ADC_SD');
+INSERT INTO signalgroup (name) VALUES ('BSL');
+INSERT INTO signalgroup (name) VALUES ('DMA');
+INSERT INTO signalgroup (name) VALUES ('CAPTIVATE');
+INSERT INTO signalgroup (name) VALUES ('LCD');
+INSERT INTO signalgroup (name) VALUES ('USCI_A');
+INSERT INTO signalgroup (name) VALUES ('USCI_B');
+
+COMMIT TRANSACTION;
+
 -- Table: signal
 CREATE TABLE IF NOT EXISTS signal (
-    id            TEXT PRIMARY KEY ON CONFLICT ROLLBACK
-                       NOT NULL,
-    desc          TEXT NOT NULL,
-    feature_group TEXT CONSTRAINT FK_SIGNAL_FEATURE REFERENCES feature (signal_group) 
+    id      TEXT PRIMARY KEY ON CONFLICT ROLLBACK
+                 NOT NULL,
+    desc    TEXT NOT NULL,
+    signal_group TEXT CONSTRAINT FK_SIGNAL_GROUP REFERENCES signalgroup (name) 
 )
 STRICT;
