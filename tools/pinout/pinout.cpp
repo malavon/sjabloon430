564343c88516d8311514b912ca2ac652b7b666ed
Benny Goemans <git@malavon.eu>
2025-05-22 22:12:21 +0200

    DB Tools Pinout: DB import status now scrolls if need be, clearer ERROR as well
    By scrolling the status message if need be, there won't be too many lines
    displayed for the screen. Of course this does mean that previous errors are not
    visible. If there are many more files in the future, it might be best to
    display either only error-ing files or display all files on a single line
    except the ones with errors.
    
    Another option might be to display files that contain errors first by moving
    the cursor back, although that might be a mess if the window has actually
    scrolled further.

--- tools/pinout/pinout.cpp
+++ tools/pinout/pinout.cpp
@@ -38,19 +38,20 @@ int main() {
 		BorderedWindow top(WIN_TOP_HEIGHT, COLS - WIN_CONFIGSET_WIDTH, 0, 0);
 		// TODO: no border, separate with hline or something?
 		Window pins(LINES - WIN_TOP_HEIGHT, COLS - WIN_CONFIGSET_WIDTH, WIN_TOP_HEIGHT, 0);
+		pins.optionScrollable(Toggle::ON);
 
-		int l = 20;
 		Window &statusWin = pins;
-		statusWin.add(l++, 0, "Imported SQLite DB from files:");
+		statusWin.add(0, 0, "Imported SQLite DB from files:\n");
 		dbf::importDatabase(db, [&](const string &filename, const char *error) {
-			statusWin.add(l++, 2, '"');
+			statusWin.add("   \"");
 			statusWin.add(filename);
 			statusWin.add('"');
 
 			if ( error != nullptr ) {
-				statusWin.add(' ');
+				statusWin.add(" ERR: ");
 				statusWin.add(error);
 			}
+			statusWin.add('\n'); // scrolls if too many lines for window without possibly erroring
 		});
 		db::DatabaseTotals totals = db::countTotals(db);
 		statusWin.paint();
