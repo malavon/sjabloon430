e81fb1b39cc4e2707142533c175144c2dd61a14d
Benny Goemans <git@malavon.eu>
2025-05-22 02:17:56 +0200

    DB Tools Pinout: signal description column-limited display
    When a description is wider than the available width it needs to be limited.
    The description is dynamically cut-off and appended with a few dots in order to
    indicate that it has been cut off.

--- tools/pinout/ui.cpp
+++ tools/pinout/ui.cpp
@@ -53,8 +53,7 @@ class PinsetEventer : public FormEventHandler {
 
 		// calculate description field to reach column 80, with a sensible minimum width
 		// but ensure that the window can fit it (although application requires 80 cols minimum)
-		int maxX = getmaxx(static_cast<WINDOW *>(window));
-		int descFieldWidth = max(FIELD_WIDTH_DESC, min(80, maxX) - descColumn - 1);
+		int descFieldWidth = max(FIELD_WIDTH_DESC, min(80, window.size().cols) - descColumn - 1);
 
 		Field descField(1, descFieldWidth, row, descColumn);
 		descField.optionsActiveAndEditable(Toggle::OFF);
@@ -193,9 +192,17 @@ void drawPinSet(Window &win, int &row, const vector<Package> &pkgs, unordered_ma
 	}
 
 	col++;
-	for ( const string &sgn : pv.cview.signals ) {
+	int descCol = col + FIELD_WIDTH_SIGNAL + 1;
+	// cut off descriptions if need be
+	size_t maxDescLength = std::max(FIELD_WIDTH_DESC, win.size().cols - descCol);
+	const string CUT_CHARS = "...";
+	for ( const string &sgn : pv[0] ) {
 		win.add(row, col, sgn);
-		win.add(row, col + FIELD_WIDTH_SIGNAL + 1, signals[sgn]);
+		if ( signals[sgn].length() > maxDescLength ) {
+			win.add(row, descCol, signals[sgn].substr(0, maxDescLength - CUT_CHARS.length()) + CUT_CHARS);
+		} else {
+			win.add(row, descCol, signals[sgn]);
+		}
 		row++;
 	}
 }
