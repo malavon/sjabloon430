7136c6e83cdf0468096b81e20be4b16ecc18270f
Benny Goemans <git@malavon.eu>
2025-05-24 02:15:04 +0200

    DB Tools Pinout: export sql with update statements & pinout orderable export
    Not all exports can be created as insert statements, sometimes updates will be
    necessary. This generic update statement generator adds this functionality.
    
    More specifically, links between orderables (thus devices & datasheets) and
    pinsets (and thus signalsets and signals) need to be updated since all records
    are already present in the database.
    
    Ordering by pinset_id and device.model is important to group related statements
    together. Sharing pinsets means the devices have the same pin numbers and many
    orderables sharing the same packages (and config set - not yet implemented)
    belong together.

--- tools/pinout/database-files.cpp
+++ tools/pinout/database-files.cpp
@@ -62,6 +62,54 @@ string insertStringFromResultSet(sqlite3_stmt *stmt) {
 	return insert;
 }
 
+// creates an update string from a resultset, first column is required to be the key on which to update!
+string updateStringFromResultSet(sqlite3_stmt *stmt) {
+	int columns = sqlite3_column_count(stmt);
+	if ( columns <= 1 ) {
+		return "ERROR NOT ENOUGH COLUMNS";
+	}
+
+	const char *table = sqlite3_column_table_name(stmt, 0);
+	string sql = "UPDATE ";
+	sql += table;
+	string updates = " SET ";
+	string condition = " WHERE ";
+
+	columns = sqlite3_data_count(stmt); // let's hope it's the same as before ...
+	// TODO: values
+	string sub;
+	for ( int c = 0; c < columns; c++ ) {
+		sub = (c > 1) ? "," : ""; // first is id, second is first update, only second update requires a comma
+		const char *origin = sqlite3_column_origin_name(stmt, c); // column name from DB
+		if ( origin == nullptr ) {
+			const char *col = sqlite3_column_name(stmt, c); // column name from AS-statement
+			sub += (col == nullptr) ? "ERROR_NO_COL_NAME" : col;
+		} else {
+			sub += origin;
+		}
+		sub += " = ";
+		const char *type = sqlite3_column_decltype(stmt, c);
+		const unsigned char *value = sqlite3_column_text(stmt, c);
+		if ( value == nullptr ) {
+			sub += "NULL"; // only allowed for updates, not condition ...
+		} else if ( strcmp(type, "INTEGER") == 0 || strcmp(type, "FLOAT") == 0 ) {
+			sub += reinterpret_cast<const char *>(value);
+		} else {
+			sub += "'";
+			sub += reinterpret_cast<const char *>(value);
+			sub += "'";
+		}
+
+		if ( c == 0 ) {
+			condition += sub;
+		} else {
+			updates += sub;
+		}
+	}
+
+	return sql + updates + condition + ";";
+}
+
 void exportFromPrepStmt(sqlite3_stmt *stmt, const string fileName, const ExportConfig &config) {
 	std::filesystem::path outFile(DB_DIRECTORY);
 	outFile /= fileName;
@@ -83,13 +131,139 @@ void exportFromPrepStmt(sqlite3_stmt *stmt, const string fileName, const ExportC
 	}
 
 	while ( SQLITE_ROW == sqlite3_step(stmt) ) {
-		out << insertStringFromResultSet(stmt) << endl;
+		if ( config.sql == ExportConfig::SQL::INSERT ) {
+			out << insertStringFromResultSet(stmt) << endl;
+		} else {
+			out << updateStringFromResultSet(stmt) << endl;
+		}
 	}
 
 	if ( config.tx == ExportConfig::Tx::BOTH || config.tx == ExportConfig::Tx::COMMIT ) {
 		out << endl << "COMMIT TRANSACTION;" << endl;
 	}
 	sqlite3_finalize(stmt);
+	out.close();
+}
+
+// sub-functions for exports of pinout data
+void exportPinoutSignalsets(sqlite3 *db, const string &datasheetId, const string filename, ExportConfig expConf) {
+	static const char *QUERY = "SELECT ss.* "
+				   "FROM signalset ss "
+				   "INNER JOIN pinset_signalset psss ON psss.signalset_id = ss.id "
+				   "INNER JOIN pinset ps ON ps.id = psss.pinset_id "
+				   "WHERE ps.id IN ("
+				   "	SELECT pinset_id "
+				   "	FROM orderable o "
+				   "	INNER JOIN device d ON d.model = o.device_id "
+				   "	WHERE datasheet_id = ? "
+				   "	GROUP BY pinset_id) "
+				   "GROUP BY ss.id "
+				   "ORDER BY datasheet_idx ASC"; // no other columns needed, only 1 datasheet
+	static sqlite3_stmt *stmt;
+	if ( stmt == nullptr ) {
+		prepare(db, &stmt, QUERY);
+	}
+
+	sqlite3_reset(stmt);
+	sqlite3_bind_text(stmt, 1, datasheetId.c_str(), -1, SQLITE_STATIC);
+	exportFromPrepStmt(stmt, filename, expConf);
+}
+
+void exportPinoutSignalsetSignalLinks(sqlite3 *db, const string &datasheetId, const string filename, ExportConfig expConf) {
+	static const char *QUERY = "SELECT sss.signalset_id, sss.idx, sss.signal_id "
+				   "FROM signalset_signal sss "
+				   "INNER JOIN signalset ss ON ss.id = sss.signalset_id "
+				   "INNER JOIN pinset_signalset psss ON psss.signalset_id = ss.id "
+				   "WHERE psss.pinset_id IN ("
+				   "	SELECT pinset_id "
+				   "	FROM orderable o "
+				   "	INNER JOIN device d ON d.model = o.device_id "
+				   "	WHERE datasheet_id = ? "
+				   "	GROUP BY pinset_id) "
+				   "GROUP BY sss.signalset_id, sss.signal_id "
+				   "ORDER BY sss.signalset_id ASC, sss.idx ASC";
+	static sqlite3_stmt *stmt;
+	if ( stmt == nullptr ) {
+		prepare(db, &stmt, QUERY);
+	}
+
+	sqlite3_reset(stmt);
+	sqlite3_bind_text(stmt, 1, datasheetId.c_str(), -1, SQLITE_STATIC);
+	exportFromPrepStmt(stmt, filename, expConf);
+}
+
+void exportPinoutPinsets(sqlite3 *db, const string &datasheetId, const string filename, ExportConfig expConf) {
+	static const char *QUERY = "SELECT ps.* "
+				   "FROM pinset ps "
+				   "WHERE id IN ( "
+				   "	SELECT pinset_id "
+				   "	FROM orderable o "
+				   "	INNER JOIN device d ON d.model = o.device_id "
+				   "	WHERE datasheet_id = ?) "
+				   "ORDER BY id ASC";
+	static sqlite3_stmt *stmt;
+	if ( stmt == nullptr ) {
+		prepare(db, &stmt, QUERY);
+	}
+
+	sqlite3_reset(stmt);
+	sqlite3_bind_text(stmt, 1, datasheetId.c_str(), -1, SQLITE_STATIC);
+	exportFromPrepStmt(stmt, filename, expConf);
+}
+
+void exportPinoutPinsetSignalsetLinks(sqlite3 *db, const string &datasheetId, const string filename, ExportConfig expConf) {
+	static const char *QUERY = "SELECT psss.* "
+				   "FROM pinset_signalset psss "
+				   "INNER JOIN pinset ps ON ps.id = psss.pinset_id "
+				   "WHERE ps.id IN ("
+				   "	SELECT pinset_id "
+				   "	FROM orderable o "
+				   "	INNER JOIN device d ON d.model = o.device_id "
+				   "	WHERE datasheet_id = ?) "
+				   "ORDER BY pinset_id ASC, signalset_id ASC, pin_bga_row ASC, pin_number ASC";
+	static sqlite3_stmt *stmt;
+	if ( stmt == nullptr ) {
+		prepare(db, &stmt, QUERY);
+	}
+
+	sqlite3_reset(stmt);
+	sqlite3_bind_text(stmt, 1, datasheetId.c_str(), -1, SQLITE_STATIC);
+	exportFromPrepStmt(stmt, filename, expConf);
+}
+
+void exportPinoutPinsetOrderableLinks(sqlite3 *db, const string &datasheetId, const string filename, ExportConfig expConf) {
+	// order on pinset_id is first because they're grouped together
+	// order including d.model because names are not always logical
+	static const char *QUERY = "SELECT name, pinset_id "
+				   "FROM orderable o "
+				   "INNER JOIN device d ON d.model = o.device_id "
+				   "WHERE pinset_id IS NOT NULL "
+				   "AND d.datasheet_id = ? "
+				   "ORDER BY pinset_id ASC, d.model ASC, o.name ASC";
+
+	static sqlite3_stmt *stmt;
+	if ( stmt == nullptr ) {
+		prepare(db, &stmt, QUERY);
+	}
+
+	sqlite3_reset(stmt);
+	sqlite3_bind_text(stmt, 1, datasheetId.c_str(), -1, SQLITE_STATIC);
+	exportFromPrepStmt(stmt, filename, expConf);
+}
+
+void exportPinoutData(sqlite3 *db, const string &datasheetId) {
+	string lower = datasheetId; // is there no single-function for strings or even const char*??
+	for ( char &c : lower ) {
+		c = std::tolower(c);
+	}
+	const string file = "99_" + lower + ".sql";
+	exportPinoutSignalsets(db, datasheetId, file, ExportConfig{.appendFile = false, .tx = ExportConfig::Tx::BEGIN});
+	exportPinoutSignalsetSignalLinks(db, datasheetId, file, ExportConfig{.appendFile = true, .tx = ExportConfig::Tx::NONE});
+	exportPinoutPinsets(db, datasheetId, file, ExportConfig{.appendFile = true, .tx = ExportConfig::Tx::NONE});
+	exportPinoutPinsetSignalsetLinks(db, datasheetId, file, ExportConfig{.appendFile = true, .tx = ExportConfig::Tx::NONE});
+	exportPinoutPinsetOrderableLinks(
+	    db, datasheetId, file,
+	    ExportConfig{.appendFile = true, .tx = ExportConfig::Tx::COMMIT, .sql = ExportConfig::SQL::UPDATE});
 }
 
 void exportSignals(sqlite3 *db) {
