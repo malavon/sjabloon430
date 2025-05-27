c8ef864c35f40c270f2a735d04b08e939d5f0cf2
Benny Goemans <git@malavon.eu>
2025-05-27 02:53:08 +0200

    MARKER COMMIT: up to here MVP without parents/configsets (maybe some more bugs :p)

--- tools/db/99_slas942.sql
+++ tools/db/99_slas942.sql
@@ -287,4 +287,5 @@ UPDATE orderable SET pinset_id = 4 WHERE name = 'MSP430FR2632IYQWT';
 UPDATE orderable SET pinset_id = 4 WHERE name = 'MSP430FR2633IYQWR';
 UPDATE orderable SET pinset_id = 4 WHERE name = 'MSP430FR2633IYQWT';
 
+
 COMMIT TRANSACTION;
