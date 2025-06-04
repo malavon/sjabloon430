8febe620562f3ed1a94008ce9133f91078ba4524
Benny Goemans <git@malavon.eu>
2025-06-05 01:36:15 +0200

    multi-configset conversion from and to DB?

--- tools/pinout/ui.hpp
+++ tools/pinout/ui.hpp
@@ -34,7 +34,7 @@ class Configset {
 		return vector<Package>(pkgs.begin(), pkgs.end());
 	}
 
-	const vector<db::Orderable> orderables() const {
+	vector<db::Orderable> orderables() const {
 		return odbls;
 	}
 
