02872b85ab7e50e49a4592b76891871871d576b3
Benny Goemans <git@malavon.eu>
2025-06-06 21:02:47 +0200

    QUICK FIX: adding new signals at end crashed due to ref issue BUT DOES NOT REALLY WORK NOTHING IS SAVED SINCE ADDING CONFIGSETS?

--- tools/pinout/ui.cpp
+++ tools/pinout/ui.cpp
@@ -373,6 +373,9 @@ void drawPinSetEditingWindow(Window &win, PinSetView &vw) {
 	// at last option editing means inserting a new one
 	if ( vw.editIdx == static_cast<int>(vw.pinViews.size()) ) {
 		PinView pv;
+		for ( size_t i = vw.csets.size(); i > 0; i-- ) {
+			pv.cviews.push_back(PinView::ConfigView());
+		}
 		scrollToAccomodate(win, MAX_SIGNALS + 1 /* header */, row);
 		drawPinSetHeader(win, row++, vw.pkgs);
 		editPinSet(win, row, vw.pkgs, vw.signalDescs, pv);
