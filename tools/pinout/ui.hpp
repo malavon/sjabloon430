07e52ef1f361bfa5b400c059b1f56084f2d38d60
Benny Goemans <git@malavon.eu>
2025-06-08 03:08:48 +0200

    TEMP? back to displaying configsets at the right, can create a new one [mockup]

--- tools/pinout/ui.hpp
+++ tools/pinout/ui.hpp
@@ -126,10 +126,10 @@ void editPinSet(Window &, int &row, const vector<string> &pkgs, unordered_map<st
 
 // Window drawing functions
 void drawPinSetEditingWindow(Window &win, PinSetView &pinView);
-void drawSetConfigWindow(Window &, const Configset &cset);
+void drawSetConfigWindow(BorderedWindow &, int &row, const Configset &);
 void drawTopWindow(BorderedWindow &, const db::Datasheet &, const db::DatabaseTotals &ttl, const db::DatabaseTotals &sprtd);
 Configset filterForConfigset(const Configset &cset);
-void loopPinsetEditing(Window &pinset, Window &hotkeys, Window &config, ui::PinSetView &);
+void loopPinsetEditing(Window &pinset, Window &hotkeys, BorderedWindow &config, ui::PinSetView &);
 void reorderPackages(vector<Package> &pkgs); // given vector is reordered in-place
 string searchDatasheet(const unordered_map<string, string> &dsModels, const int widestModelLength);
 
