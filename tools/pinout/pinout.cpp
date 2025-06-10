cc369e7f32ed4bd3dd52e71c0320ad8fd4cec0d9
Benny Goemans <git@malavon.eu>
2025-06-10 04:02:58 +0200

    DB Tools Pinout: configset window fallback for small terminals (& LLDB)
    I already knew that the application wouldn't work when the set window outgrows
    the terminal height for some time, but until configsets were really improving
    implemented it wasn't a priority. Since it also broke debugging it became one.
    
    The code has become rather complicated with several ifs added, there might be
    a cleaner option but the functionality is there.
    The fallbacks are quite numorous:
    * leaving out the default set if all others can be rendered
    * leaving out packages of all sets (doesn't really work with package-only set)
    * leaving out headers
    * and if all else fails: add a single line per configset, without models/pkgs

--- tools/pinout/pinout.cpp
+++ tools/pinout/pinout.cpp
@@ -44,7 +44,7 @@ int main() {
 		Window pins(LINES - WIN_TOP_HEIGHT, COLS - WIN_CONFIGSET_WIDTH, WIN_TOP_HEIGHT, 0);
 		pins.optionScrollable(Toggle::ON);
 		BorderedWindow configs(LINES, WIN_CONFIGSET_WIDTH, 0, COLS - WIN_CONFIGSET_WIDTH);
-		configs.setTitle("Config: default");
+		configs.setTitle("Configsets");
 
 		Window &statusWin = pins;
 		statusWin.add(0, 0, "Imported SQLite DB from files:\n");
