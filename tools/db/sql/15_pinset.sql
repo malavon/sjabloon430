6442ba1c07484126c762f34664a9a9da700fc99e
Benny Goemans <git@malavon.eu>
2025-06-15 20:42:24 +0200

    DB: remove pin count from pinset
    Having removed it from the pinout code, it can now be removed from the database
    as well. Exported SQL files should be re-exported, but not in this commit.

--- tools/db/sql/15_pinset.sql
+++ tools/db/sql/15_pinset.sql
@@ -10,7 +10,6 @@ CREATE TABLE IF NOT EXISTS pinset (
                       NOT NULL,
     parent_id INTEGER REFERENCES pinset (id) ON DELETE RESTRICT
                                              ON UPDATE CASCADE,
-    pins      INTEGER DEFAULT (0),
     comment   TEXT
 )
 STRICT;
