9d4cdbfc1cdd1078699c5e3b386895b4a26dfe35
Benny Goemans <git@malavon.eu>
2025-06-16 05:02:59 +0200

    DB Tools Pinout: remove gaps in pinset & signalset ids
    In complex datasheets it is possible to do some editing, save and pick up
    further editing later. When this is used to add configsets at a later stage or
    moving signals between configsets a new signalset will be added. The old one
    may be left empty. This empty signalset is then removed from the SQL exports,
    leaving a gap in the ids.
    
    TODO: this feature (at least when it's corrected to remove unlinked signalsets)
    will result in more difficult merges? Updating it manually is a safer option?

--- tools/pinout/database.hpp
+++ tools/pinout/database.hpp
@@ -137,6 +137,9 @@ int saveOrUpdatePinset(sqlite3 *db, Pinset &ps);	 // assumes signalsets are all 
 int saveOrUpdateSignalset(sqlite3 *db, Signalset &sets); // assumes signals are all in DB!
 int saveSignals(sqlite3 *db, unordered_map<string, string> signals);
 
-}}}} // namespace sjabloon430::tools::pinout::db
+/* Beautification ... */
+int removeGapsInPinsetIds(sqlite3 *db);
+int removeGapsInSignalsetIds(sqlite3 *db);
 
+}}}} // namespace sjabloon430::tools::pinout::db
 #endif // SJABLOON430_TOOLS_PINOUT_DATABASE_HPP
