a27f86d2b967663bac9d9f8a1d38f050e6da6825
Benny Goemans <git@malavon.eu>
2025-06-17 05:19:38 +0200

    DB Tools: export device features
    Device features are currently not used, but they are inserted by the device
    tool and as such need to be stored. They may still be susceptible to changes
    though.

--- tools/pinout/database-files.cpp
+++ tools/pinout/database-files.cpp
@@ -37,7 +37,8 @@ string insertStringFromResultSet(sqlite3_stmt *stmt, const vector<int> &widths =
 string updateStringFromResultSet(sqlite3_stmt *stmt, const vector<int> &widths = {}, int whereColumns = 1);
 
 // partial export functions for datasheet data
-void exportDevicesFor(sqlite3 *db, const string &datasheetId, const string filename, const ExportConfig &expConf);
+void exportDevicesFor(sqlite3 *db, const string &datasheetId, const string filename, const ExportConfig &);
+void exportDeviceFeaturesFor(sqlite3 *db, const string &datasheetId, const string filename, const ExportConfig &);
 void exportOrderablesFor(sqlite3 *db, const string &datasheetId, const string filename, const ExportConfig &);
 void exportOrderablePinsetsFor(sqlite3 *db, const string &datasheetId, const string filename, const ExportConfig &);
 void exportPinsetsFor(sqlite3 *db, const string &datasheetId, const string filename, const ExportConfig &);
@@ -236,6 +237,22 @@ void exportDevicesFor(sqlite3 *db, const string &datasheetId, const string filen
 	exportFromPrepStmt(stmt, filename, expConf);
 }
 
+void exportDeviceFeaturesFor(sqlite3 *db, const string &datasheetId, const string filename, const ExportConfig &expConf) {
+	static const char *QUERY = "SELECT device_id, feature_id, param1, param2, param3, df.comment "
+				   "FROM device_feature df "
+				   "INNER JOIN device d ON df.device_id = d.model "
+				   "WHERE d.datasheet_id = ? "
+				   "ORDER BY df.device_id ASC";
+	static sqlite3_stmt *stmt;
+	if ( stmt == nullptr ) {
+		prepare(db, &stmt, QUERY);
+	}
+
+	sqlite3_reset(stmt);
+	sqlite3_bind_text(stmt, 1, datasheetId.c_str(), -1, SQLITE_STATIC);
+	exportFromPrepStmt(stmt, filename, expConf);
+}
+
 void exportOrderablesFor(sqlite3 *db, const string &datasheetId, const string filename, const ExportConfig &expConf) {
 	// cannot use select *, pinset_id is forced to NULL so it can be updated with an update statement AFTER
 	// pinset export!
@@ -372,6 +389,7 @@ void exportDataForDatasheet(sqlite3 *db, const string &datasheetId) {
 	ExportConfig middle = start.appendOverride(true).txOverride(ExportConfig::Tx::NONE);
 	ExportConfig end = middle.txOverride(ExportConfig::Tx::COMMIT);
 	exportDevicesFor(db, datasheetId, file, start);
+	exportDeviceFeaturesFor(db, datasheetId, file, middle);
 	exportOrderablesFor(db, datasheetId, file, middle);
 	exportPinsetsFor(db, datasheetId, file, middle);
 	exportSignalsetsFor(db, datasheetId, file, middle);
