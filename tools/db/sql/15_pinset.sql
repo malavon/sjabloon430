cb655529fd5c1ebadb922f8b6edd07a2722db086
Benny Goemans <git@malavon.eu>
2025-06-19 03:03:50 +0200

    DB: pin count in pinset no longer mandatory (no NOT NULL, but default value)

--- tools/db/sql/15_pinset.sql
+++ tools/db/sql/15_pinset.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS pinset (
                       NOT NULL,
     parent_id INTEGER REFERENCES pinset (id) ON DELETE RESTRICT
                                              ON UPDATE CASCADE,
-    pins      INTEGER NOT NULL,
+    pins      INTEGER DEFAULT (0),
     comment   TEXT
 )
 STRICT;
